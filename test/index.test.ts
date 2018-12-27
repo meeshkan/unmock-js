47be9b523d9f4cd2b80e9b41801fbe1e7e054165
Mike Solomon <mike@mikesolomon.org>
2018-12-27 14:12:11 +0200

    Adds additional api calls to test
    Bumps version
    
    Adds things to ignore to npm
    
    Bumps version

--- test/index.test.ts
+++ test/index.test.ts
@@ -12,6 +12,12 @@ beforeAll(async () => {
 afterAll(async () => await kcomnu());
 
 test("unmock end to end", async () => {
-  const result = await axios("https://www.behance.net/v2/projects?api_key=u_n_m_o_c_k_200");
-  expect(result.data.projects[0].id).toBeGreaterThan(0);
+  const { data: { projects } }  = await axios("https://www.behance.net/v2/projects?api_key=u_n_m_o_c_k_200");
+  expect(typeof projects[0].id).toBe("number");
+  const { data } =
+    await axios(`https://www.behance.net/v2/projects/${projects[0].id}?api_key=u_n_m_o_c_k_200`);
+  expect(data.id).toBe(projects[0].id);
+  const { data: { comments } } =
+    await axios(`https://www.behance.net/v2/projects/${projects[0].id}/comments?api_key=u_n_m_o_c_k_200`);
+  expect(typeof comments[0].comment).toBe("string");
 });
