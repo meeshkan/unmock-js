d262f4be074237b4e13996eae9b32cec988b1a68
Idan Tene <idan.tene@gmail.com>
2019-04-22 01:24:04 +0300

    Own version of snapshot

--- packages/unmock-core/src/snapshot.ts
+++ packages/unmock-core/src/snapshot.ts
@@ -1,6 +1,11 @@
 import expect from "expect";
 import * as fs from "fs";
+import * as mkdirp from "mkdirp";
 import * as path from "path";
+
+const SNAPSHOTS_FOLDER = path.join(process.cwd(), "__snapshots__");
+
+const CLEARED_SNAPSHOT_FILES: string[] = [];
 
 // Add snapshot handler
 expect.addSnapshotSerializer({
@@ -8,6 +13,7 @@ expect.addSnapshotSerializer({
   // This serializer only applies to values that have these attributes:
   test: (val: any) => val && val.hash && val.host && val.method && val.path,
 });
+
 declare global {
   namespace expect {
     // tslint:disable-next-line: interface-name
@@ -16,24 +22,37 @@ declare global {
     }
   }
 }
+
 expect.extend({
   snapshot(obj: any) {
-    const snapFile = path.join("__snapshots__", `${this.currentTestName}.snap`);
+    const snapFile = path.join(SNAPSHOTS_FOLDER, `${path.basename(this.testPath)}.snap`);
+    if (!fs.existsSync(SNAPSHOTS_FOLDER)) {
+      mkdirp.sync(SNAPSHOTS_FOLDER);
+    }
     let contents: {[testCallNumber: string]: string} = {};
     if (fs.existsSync(snapFile)) {
-      contents = require(snapFile);
+      if (CLEARED_SNAPSHOT_FILES.indexOf(snapFile) === -1) {
+        // First we delete previous snapshots, then we continuously update them.
+        CLEARED_SNAPSHOT_FILES.push(snapFile);
+        fs.unlinkSync(snapFile);
+      } else {
+        contents = require(snapFile);
+      }
     }
     const numOfCalls = Object.keys(contents).length + 1;
-    contents[`${this.currentTestName} ${numOfCalls}`] = JSON.stringify(obj, undefined, 2);
-    fs.writeFileSync(snapFile, Object.keys(contents).map(
-      (key: string) => "exports['" + key + "'] = " + contents[key] + ";"),
-    );
+    contents[`${this.currentTestName} ${numOfCalls}`] = obj;
+    const stringContents = Object.keys(contents)
+      .map((key: string) => "exports['" + key + "'] = " + JSON.stringify(contents[key], undefined, 2) + ";\n")
+      .join("\n");
+    fs.writeFileSync(snapFile, stringContents);
+    return {pass: true};
   },
 });
 
 export const snapshot = (obj: any) => {
   if (process.env.JEST_WORKER_ID !== undefined) {
-    expect(obj).snapshot(); // Creates the snapshot with the above serializer
+    // IDE might highlight `snapshot`, but it is well declared above, so we can ignore this...
+    expect(obj).snapshot();  // Creates the snapshot with the above serializer
     // Reserved to add snapshots for other backends
   }
 };
