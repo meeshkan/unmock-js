95a379ce16019b973e43f29f2346a7c2b55138c7
Mike Solomon <mike@mikesolomon.org>
2019-04-25 09:17:27 +0300

    Adds hash

---
+++ packages/unmock-hash/src/jsonType.ts
@@ -0,0 +1,4 @@
+export type IJSONPrimitive = string | number | boolean | null;
+export type IJSONValue = IJSONPrimitive | IJSONObject | IJSONArray;
+export interface IJSONObject { [key: string]: IJSONPrimitive; }
+export interface IJSONArray extends Array<IJSONValue> {}
