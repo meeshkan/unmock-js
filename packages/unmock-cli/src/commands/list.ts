0bbc95ddfa7fbfa80e43a164957e9a3edb047aee
Idan Tene <idan.tene@gmail.com>
2019-04-26 12:13:37 +0300

    Restructure imports and package structure

---
+++ packages/unmock-cli/src/commands/list.ts
@@ -0,0 +1,36 @@
+import glob from "glob";
+import { WinstonLogger } from "unmock-node";
+
+const removeTrailingNumber = (s: string) =>
+  s
+    .split(" ")
+    .slice(0, -1)
+    .join(" ");
+
+export const list = () => {
+  const logger = new WinstonLogger();
+  glob("__snapshots__/**/*.snap", (e, matches) => {
+    if (e) {
+      throw e;
+    }
+    matches.forEach((match) => {
+      logger.log(match.substring(0, match.length - 5).substring(14));
+      const snapshots = require(`${process.cwd()}/${match}`);
+      const tests = Object.keys(snapshots)
+        .map((name) => ({ [removeTrailingNumber(name)]: new Array<string>() }))
+        .reduce((a, b) => ({ ...a, ...b }), {});
+      Object.keys(snapshots).forEach((name) => {
+        tests[removeTrailingNumber(name)].push(name);
+      });
+      Object.keys(tests).forEach((test) => {
+        logger.log(`  ${test}`);
+        tests[test].forEach((call) => {
+          logger.log(`    hash: ${snapshots[call].hash}`);
+          logger.log(`      method: ${snapshots[call].method}`);
+          logger.log(`      host: ${snapshots[call].host}`);
+          logger.log(`      path: ${snapshots[call].path}`);
+        });
+      });
+    });
+  });
+};
--- packages/unmock-cli/src/commands/open.ts
+++ packages/unmock-cli/src/commands/open.ts
@@ -2,13 +2,13 @@ import child_process from "child_process";
 import fs from "fs";
 import { WinstonLogger } from "unmock-node";
 
-export default (hash: string, options: any) => {
+export const open = (hash: string, options: any) => {
   const logger = new WinstonLogger();
   const editor = options.editor || process.env.EDITOR || "vi";
   const path = `${process.cwd()}/.unmock/save/${hash}/response.json`;
   if (fs.existsSync(path)) {
     const child = child_process.spawn(editor, [path], {
-        stdio: "inherit",
+      stdio: "inherit",
     });
     child.on("exit", () => {
       logger.log(`Done editing ${hash}.`);
