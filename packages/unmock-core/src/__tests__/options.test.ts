df8ced95d1430843bcaa21d4fe218e34bca26fdd
Mike Solomon <mike@mikesolomon.org>
2019-05-04 18:06:03 +0300

    Makes sure that failing persistence fails

--- packages/unmock-core/src/__tests__/options.test.ts
+++ packages/unmock-core/src/__tests__/options.test.ts
@@ -15,3 +15,43 @@ describe("tests whitelist", () => {
     expect(opts.isWhitelisted("12.5.0.1.2.3.4.1")).toEqual(true);
   });
 });
+
+test("forget to set persistence", () => {
+  const opts = new UnmockOptions();
+  expect(() => {
+    opts.persistence.getPath();
+  }).toThrow();
+  expect(() => {
+    opts.persistence.hasHash("foo");
+  }).toThrow();
+  expect(() => {
+    opts.persistence.loadAuth();
+  }).toThrow();
+  expect(() => {
+    opts.persistence.loadMeta("foo");
+  }).toThrow();
+  expect(() => {
+    opts.persistence.loadRequest("foo");
+  }).toThrow();
+  expect(() => {
+    opts.persistence.loadResponse("foo");
+  }).toThrow();
+  expect(() => {
+    opts.persistence.loadToken();
+  }).toThrow();
+  expect(() => {
+    opts.persistence.loadUserId();
+  }).toThrow();
+  expect(() => {
+    opts.persistence.saveAuth("foo");
+  }).toThrow();
+  expect(() => {
+    opts.persistence.saveMeta("foo", {});
+  }).toThrow();
+  expect(() => {
+    opts.persistence.saveRequest("foo", {});
+  }).toThrow();
+  expect(() => {
+    opts.persistence.saveResponse("foo", {});
+  }).toThrow();
+});
