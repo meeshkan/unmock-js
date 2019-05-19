7c399f8ed3e5e0b9a5a49b111a08e731d7a8b18c
Mike Solomon <mike@meeshkan.com>
2019-05-19 22:46:21 +0300

    Hotfix of User-Agent so that it is excluded in lowercase and capital.

--- packages/unmock-core/src/constants.ts
+++ packages/unmock-core/src/constants.ts
@@ -1,5 +1,5 @@
 export const UNMOCK_HOST = "api.unmock.io";
 export const UNMOCK_PORT = "443";
-export const DEFAULT_IGNORE_HEADER = { headers: "\\w*User-Agent\\w*" };
+export const DEFAULT_IGNORE_HEADER = { headers: "\\w*User-Agent\\w*|\\w*user-agent\\w*" };
 export const UNMOCK_UA_HEADER_NAME = "X-Unmock-Client-User-Agent";
 export const MOSES = "MOSES"; // Default user id (anonymous)
