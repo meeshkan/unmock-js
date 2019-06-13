830a7e8d2062358e8e5ccacd6d6cae617c03903c
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-06-13 13:43:14 +0300

    Reorganize and add tests.

--- packages/unmock-core/src/__tests__/matcher.test.ts
+++ packages/unmock-core/src/__tests__/matcher.test.ts
@@ -18,31 +18,70 @@ const mockResponse: ISerializedResponse = {
 };
 
 describe("http request matcher", () => {
-  it("returns undefined if no mocks are available", () => {
-    const httpRequestMatcher = httpRequestMatcherFactory(() => []);
-    const response = httpRequestMatcher(request);
-    expect(response).toBeUndefined();
+  describe("with no available mocks", () => {
+    it("returns undefined if no mocks are available", () => {
+      const httpRequestMatcher = httpRequestMatcherFactory(() => []);
+      const response = httpRequestMatcher(request);
+      expect(response).toBeUndefined();
+    });
   });
-  it("returns undefined if hostname does not match", () => {
-    const requestWithDifferentHostname = {
-      ...request,
-      host: "bar.foo.com",
-    };
-    const mocks = [
-      { request: requestWithDifferentHostname, response: mockResponse },
-    ];
-    const httpRequestMatcher = httpRequestMatcherFactory(() => mocks);
-    const matchedResponse = httpRequestMatcher(request);
-    expect(matchedResponse).toBeUndefined();
+
+  describe("with literal mocks", () => {
+    it("returns undefined if hostname does not match", () => {
+      const requestWithDifferentHostname = {
+        ...request,
+        host: "bar.foo.com",
+      };
+      const mocks = [
+        { request: requestWithDifferentHostname, response: mockResponse },
+      ];
+      const httpRequestMatcher = httpRequestMatcherFactory(() => mocks);
+      const matchedResponse = httpRequestMatcher(request);
+      expect(matchedResponse).toBeUndefined();
+    });
+    it("returns the correct response for exact match", () => {
+      const requestCopy: ISerializedRequest = {
+        ...request,
+      };
+      const mocks = [{ request: requestCopy, response: mockResponse }];
+      const httpRequestMatcher = httpRequestMatcherFactory(() => mocks);
+      const matchedResponse = httpRequestMatcher(request);
+      expect(matchedResponse).toEqual(mockResponse);
+    });
   });
-  it("returns the correct response for exact match", () => {
-    const requestCopy: ISerializedRequest = {
-      ...request,
-    };
-    const mocks = [{ request: requestCopy, response: mockResponse }];
-    const httpRequestMatcher = httpRequestMatcherFactory(() => mocks);
-    const matchedResponse = httpRequestMatcher(request);
-    expect(matchedResponse).toEqual(mockResponse);
+
+  describe("with a mock containing regexes", () => {
+    it("returns the correct response for regex match", () => {
+      const requestWithRegex: IMockRequest = {
+        ...request,
+        host: /.*foo\.com/,
+      };
+      const mocks = [{ request: requestWithRegex, response: mockResponse }];
+      const httpRequestMatcher = httpRequestMatcherFactory(() => mocks);
+      const matchedResponse = httpRequestMatcher(request);
+      expect(matchedResponse).toEqual(mockResponse);
+    });
+    it("returns undefined for regex mismatch", () => {
+      const requestWithRegex: IMockRequest = {
+        ...request,
+        host: /.*bar\.com/,
+      };
+      const mocks = [{ request: requestWithRegex, response: mockResponse }];
+      const httpRequestMatcher = httpRequestMatcherFactory(() => mocks);
+      const matchedResponse = httpRequestMatcher(request);
+      expect(matchedResponse).toBeUndefined();
+    });
   });
-  it("returns the correct response for hostname regex match", () => {});
+
+  describe("with a mock missing properties", () => {
+    it("matches if mock is missing all properties", () => {
+      const requestWithNoProperties: IMockRequest = {};
+      const mocks = [
+        { request: requestWithNoProperties, response: mockResponse },
+      ];
+      const httpRequestMatcher = httpRequestMatcherFactory(() => mocks);
+      const matchedResponse = httpRequestMatcher(request);
+      expect(matchedResponse).toEqual(mockResponse);
+    });
+  });
 });
