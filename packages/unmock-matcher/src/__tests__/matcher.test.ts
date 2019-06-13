f6a5a911474d4a598b394a5fdc55f65a186b8a5c
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-06-13 11:06:49 +0300

    Add two more tests.

--- packages/unmock-matcher/src/__tests__/matcher.test.ts
+++ packages/unmock-matcher/src/__tests__/matcher.test.ts
@@ -1,3 +1,4 @@
+import { ISerializedResponse } from "unmock-core";
 import { httpRequestMatcherFactory } from "..";
 
 const request = {
@@ -6,10 +7,36 @@ const request = {
   pathname: "/v1/users",
 };
 
+const mockResponse: ISerializedResponse = {
+  body: "",
+  statusCode: 200,
+};
+
 describe("http request matcher", () => {
   it("returns undefined if no mocks are available", () => {
     const httpRequestMatcher = httpRequestMatcherFactory(() => []);
     const response = httpRequestMatcher(request);
     expect(response).toBeUndefined();
+  });
+  it("returns undefined if hostname does not match", () => {
+    const requestWithDifferentHostname = {
+      ...request,
+      hostname: "https://bar.foo.com",
+    };
+    const mocks = [
+      { request: requestWithDifferentHostname, response: mockResponse },
+    ];
+    const httpRequestMatcher = httpRequestMatcherFactory(() => mocks);
+    const matchedResponse = httpRequestMatcher(request);
+    expect(matchedResponse).toBeUndefined();
+  });
+  it("returns the correct response for exact match", () => {
+    const requestCopy = {
+      ...request,
+    };
+    const mocks = [{ request: requestCopy, response: mockResponse }];
+    const httpRequestMatcher = httpRequestMatcherFactory(() => mocks);
+    const matchedResponse = httpRequestMatcher(request);
+    expect(matchedResponse).toEqual(mockResponse);
   });
 });
