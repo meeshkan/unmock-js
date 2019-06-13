3c8241c83b4142fd8f19d738b36223b4e4500f07
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-06-13 10:52:05 +0300

    Add very simple test.

--- packages/unmock-matcher/src/__tests__/matcher.test.ts
+++ packages/unmock-matcher/src/__tests__/matcher.test.ts
@@ -1,5 +1,13 @@
+import { httpRequestMatcherFactory } from "..";
+
 describe("http request matcher", () => {
-  it("works", () => {
-    expect(0).toBe(1);
+  it("returns undefined for empty mocks", () => {
+    const mocks = () => [];
+    const httpRequestMatcher = httpRequestMatcherFactory(mocks);
+    const request = {
+      hostname: "http://api.foo.com",
+    };
+    const response = httpRequestMatcher(request);
+    expect(response).toBeUndefined();
   });
 });
