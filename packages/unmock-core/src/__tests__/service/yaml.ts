d411647909fe4761801cd8996c3804180d99cbfd
Mike Solomon <mike@meeshkan.com>
2019-06-14 10:16:49 +0300

    Fixes tests

--- packages/unmock-core/src/__tests__/service/yaml.ts
+++ packages/unmock-core/src/__tests__/service/yaml.ts
@@ -6,107 +6,145 @@ import validUrl from "valid-url";
 import { IRef, resolve, schema } from "../../service/yaml";
 
 test("!arr results in a fixed-length array", () => {
-    const y = `foo: 1
-bar:
-    a: [1, 2, 3]
-    b: !arr [1, 2]
-    c:
-        d: [1, 2, arr! [3, 4]]
+  const y = `
+info:
+  name: myapi
+servers: [{url: https://myapi.unmock.io}]
+paths:
+  /foo:
+    foo: 1
+    bar:
+      a: [1, 2, 3]
+      b: !arr [1, 2]
+      c:
+        d: [1, 2, !arr [3, 4]]
 `;
-    const z = yaml.load(y, { schema });
-    const zz = resolve(z)(z, []);
-    expect(zz).toEqual({
-        foo: 1,
-        bar: {
-            a: [1,2,3],
-            b: [1,2],
-            c: { d: [1, 2, [3, 4]]}
-        }
-    });
-    expect(Object.isSealed(z.bar.b)).toBe(true);
-    expect(Object.isSealed(z.bar.b.c.d[2])).toBe(true);
+  const z = yaml.load(y, { schema });
+  const zz = resolve(z)(z, []).paths["/foo"];
+  expect(zz).toEqual({
+    foo: 1,
+    bar: {
+      a: [1,2,3],
+      b: [1,2],
+      c: { d: [1, 2, [3, 4]]}
+    }
+  });
+  expect(Object.isSealed(z.bar.b)).toBe(true);
+  expect(Object.isSealed(z.bar.b.c.d[2])).toBe(true);
 });
 
 test("!cat concatenates stuff", () => {
-    const y = `foo: 1
-bar:
-    a: !cat [1, 2, 3]
-    b: !cat [1, a, true]
+  const y = `
+info:
+  name: myapi
+servers: [{url: https://myapi.unmock.io}]
+paths:
+  /foo:
+    foo: 1
+    bar:
+      a: !cat [1, 2, 3]
+      b: !cat [1, a, true]
 `;
-    const z = yaml.load(y, { schema });
-    const zz = resolve(z)(z, []);
-    expect(zz.bar.a).toBe("123");
-    expect(zz.bar.b).toBe("1atrue");
+  const z = yaml.load(y, { schema });
+  const zz = resolve(z)(z, []).paths["/foo"];
+  expect(zz.bar.a).toBe("123");
+  expect(zz.bar.b).toBe("1atrue");
 });
 
 test("!inc includes filez", (done) => {
-    temp.track();
-    const info = temp.openSync();
-    fs.writeSync(info.fd, "q: r");
-    const y = `foo: 1
-bar:
-    a: !inc
-    b: c
+  temp.track();
+  const info = temp.openSync();
+  fs.writeSync(info.fd, "q: r");
+  const y = `
+info:
+  name: myapi
+servers: [{url: https://myapi.unmock.io}]
+paths:
+  /foo:
+    foo: 1
+    bar:
+      a: !inc '${info.path}'
+      b: c
 `;
-    const z = yaml.load(y, { schema });
-    const zz = resolve(z)(z, []);
-    expect(zz).toEqual({foo: 1, bar: { a: {q : "r"}, b: "c"}});
+  const z = yaml.load(y, { schema });
+  const zz = resolve(z)(z, []).paths["/foo"];
+  expect(zz).toEqual({foo: 1, bar: { a: {q : "r"}, b: "c"}});
 });
 
 
 test("!jsf generates correct json-schema-faker", () => {
-    const y = `foo: 1
-bar:
-    a: !jsf "internet.email"
-    b: !jsf { faker: "internet.url" }
-    c: !jsf {
-        "type": "integer",
-        "minimum": 602,
-        "maximum": 700,
-        "multipleOf": 7,
-        "exclusiveMinimum": true
-        }
+  const y = `
+info:
+  name: myapi
+servers: [{url: https://myapi.unmock.io}]
+paths:
+  /foo:
+    foo: 1
+    bar:
+      a: !jsf "internet.email"
+      b: !jsf { faker: "internet.url" }
+      c: !jsf {
+          "type": "integer",
+          "minimum": 602,
+          "maximum": 700,
+          "multipleOf": 7,
+          "exclusiveMinimum": true
+          }
 `;
-    const z = yaml.load(y, { schema });
-    const zz = resolve(z)(z, []);
-    expect(isemail.validate(zz.bar.a)).toBe(true);
-    expect(validUrl.isWebUri(zz.bar.b)).toBe(true);
-    expect(zz.bar.c).toBeGreaterThan(602);
-    expect(zz.bar.c).toBeLessThanOrEqual(700);
-    expect(zz.bar.c % 7).toBe(0);
+  const z = yaml.load(y, { schema });
+  const zz = resolve(z)(z, []).paths("/foo");
+  expect(isemail.validate(zz.bar.a)).toBe(true);
+  expect(validUrl.isWebUri(zz.bar.b)).toBe(true);
+  expect(zz.bar.c).toBeGreaterThan(602);
+  expect(zz.bar.c).toBeLessThanOrEqual(700);
+  expect(zz.bar.c % 7).toBe(0);
 });
 
 test("!oas uses OpenAPI", () => {
-    const y = `a: !oas
-    schema:
-        $ref: "#/components/schemas/Pet"
+  const y = `
+info:
+  name: myapi
+servers: [{url: https://myapi.unmock.io}]
+paths:
+  /foo:
+    foo: 1
+    bar:
+      a: !oas
+        schema:
+          $ref: "#/components/schemas/Pet"
 components:
-    schemas:
-        Pet:
-            required:
-                - id
-                - name
-            properties:
-                id:
-                    type: integer
-                    format: int64
-                name:
-                    type: string
-                tag:
-                    type: string`;
-    const z = yaml.load(y, { schema });
-    const zz = resolve(z)(z, []);
-    expect(typeof zz.a.id).toBe("number");
-    expect(typeof zz.a.name).toBe("name");
-    expect(typeof zz.a.tag).toBe("string");
-})
+  schemas:
+    Pet:
+      required:
+        - id
+        - name
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+        tag:
+          type: string
+`;
+  const z = yaml.load(y, { schema });
+  const zz = resolve(z)(z, []).paths("/foo");
+  expect(typeof zz.a.id).toBe("number");
+  expect(typeof zz.a.name).toBe("name");
+  expect(typeof zz.a.tag).toBe("string");
+});
 
 test("!ref creates a reference", () => {
-    const y = `foo: 1
-bar:
-    a: !ref foo
+  const y = `
+info:
+  name: myapi
+servers: [{url: https://myapi.unmock.io}]
+paths:
+  /foo:
+    foo: 1
+    bar: $ref foo
 `;
-    const z = yaml.load(y, { schema });
-    const zz = resolve(z)(z, []);
-    expect(zz.bar.a.ref()).toBe("foo");
+  const z = yaml.load(y, { schema });
+  const zz = resolve(z)(z, []).paths["/foo"];
+  expect(zz.bar).toBe(1);
 });
