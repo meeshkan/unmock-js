4ee0e8553f1bac8f4ce11f7cf9111087089c73f4
Mike Solomon <mike@meeshkan.com>
2019-06-14 08:22:34 +0300

    More tests

--- packages/unmock-core/src/__tests__/service/yaml.ts
+++ packages/unmock-core/src/__tests__/service/yaml.ts
@@ -1,5 +1,7 @@
+import isemail from "isemail";
 import yaml from "js-yaml";
-import { schema } from "../../service/yaml";
+import validUrl from "valid-url";
+import { resolve, schema } from "../../service/yaml";
 
 test("!arr results in a fixed-length array", () => {
     const y = `foo: 1
@@ -10,7 +12,8 @@ bar:
         d: [1, 2, arr! [3, 4]]
 `;
     const z = yaml.load(y, { schema });
-    expect(z).toEqual({
+    const zz = resolve(z)(z, []);
+    expect(zz).toEqual({
         foo: 1,
         bar: {
             a: [1,2,3],
@@ -20,4 +23,38 @@ bar:
     });
     expect(Object.isSealed(z.bar.b)).toBe(true);
     expect(Object.isSealed(z.bar.b.c.d[2])).toBe(true);
+});
+
+test("!cat concatenates stuff", () => {
+    const y = `foo: 1
+bar:
+    a: !cat [1, 2, 3]
+    b: !cat [1, a, true]
+`;
+    const z = yaml.load(y, { schema });
+    const zz = resolve(z)(z, []);
+    expect(zz.bar.a).toBe("123");
+    expect(zz.bar.b).toBe("1atrue");
+});
+
+test("!jsf generates correct json-schema-faker", () => {
+    const y = `foo: 1
+bar:
+    a: !jsf "internet.email"
+    b: !jsf { faker: "internet.url" }
+    c: !jsf {
+        "type": "integer",
+        "minimum": 602,
+        "maximum": 700,
+        "multipleOf": 7,
+        "exclusiveMinimum": true
+      }
+`;
+    const z = yaml.load(y, { schema });
+    const zz = resolve(z)(z, []);
+    expect(isemail.validate(zz.bar.a)).toBe(true);
+    expect(validUrl.isWebUri(zz.bar.b)).toBe(true);
+    expect(zz.bar.c).toBeGreaterThan(602);
+    expect(zz.bar.c).toBeLessThanOrEqual(700);
+    expect(zz.bar.c % 7).toBe(0);
 });
--- packages/unmock-core/src/service/yaml.ts
+++ packages/unmock-core/src/service/yaml.ts
@@ -1,1 +1,51 @@
+type UnmockRule = null;
+type UnmockPrimitive = number | string | boolean | null | UnmockSchema;
+type UnmockValue = UnmockPrimitive | UnmockObject | UnmockArray;
+// tslint:disable-next-line:interface-over-type-literal
+type UnmockObject = { [member: string]: UnmockValue };
+// tslint:disable-next-line:interface-name
+interface UnmockArray extends Array<UnmockValue> {}
+
+type UnmockSchema = (
+    urschema: UnmockSchema,
+    traversal: Array<string | number>,
+    hints?: {
+        protocol?: string,
+        url?: string,
+        path?: string,
+        query?: {[s: string]: string},
+        headers?: {[s: string]: string | string[] },
+        body?: string,
+        rules?: UnmockRule[],
+    }) => any;
+
+type UnmockResolver = (curschema: UnmockValue) => UnmockSchema;
+
+export const resolve: UnmockResolver = (curschema: UnmockValue) =>
+    (
+        urschema: UnmockSchema,
+        traversal: Array<string | number>,
+        hints?: {
+            protocol?: string,
+            url?: string,
+            path?: string,
+            query?: {[s: string]: string},
+            headers?: {[s: string]: string | string[]},
+            body?: string,
+            rules?: UnmockRule[],
+        },
+    ) =>
+    typeof curschema === "function" ?
+    curschema(urschema, traversal, hints) :
+    curschema instanceof Array ?
+    curschema.map((i, j) => resolve(i)(urschema, traversal.concat(j), hints)) :
+    curschema === null ?
+    null :
+    typeof curschema === "object" ?
+    Object
+        .keys(curschema)
+        .map((i) => ({i: resolve(curschema[i])(urschema, traversal.concat(i), hints)}))
+        .reduce((a, b) => ({ ...a , ...b}), {}) :
+    curschema;
+
 export const schema = {};
