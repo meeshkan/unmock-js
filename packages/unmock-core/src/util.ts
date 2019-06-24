7b60b260f010182711a83fc961d599975a362b0c
Cain (Meeshkan) <idan@meeshkan.com>
2019-06-24 09:53:24 +0300

    Adds getAtLevel tests

--- packages/unmock-core/src/util.ts
+++ packages/unmock-core/src/util.ts
@@ -18,8 +18,18 @@ export const getAtLevel = (
   level: number,
   filterCb?: (key: string, value: any) => boolean,
 ) => {
-  // Returns all nested objects at a certain level from nestedObj with additional
-  // filtering based on key and value from callback. Filtering only applies at the requested level.
+  /** Returns all nested objects at a certain level from nestedObj with additional
+   * filtering based on key and value from callback. Filtering only applies at the requested level.
+   * `level` is 0-based.
+   */
+  if (level < 0) {
+    throw new Error(`Not sure what should I find at nested level ${level}...`);
+  }
+  if (nestedObj === undefined || Object.keys(nestedObj).length === 0) {
+    throw new Error(
+      `Empty 'nestedObj' received - ${JSON.stringify(nestedObj)}`,
+    );
+  }
   let i = 0;
   let subObjects: any[] = [];
   let prevObjects: any[] = [nestedObj];
