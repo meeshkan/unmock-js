aebbde87b17c0e86b8d592eab7e845557684d70e
Cain (Meeshkan) <idan@meeshkan.com>
2019-06-27 09:53:43 +0300

    Removes DEFAULT_REST_METHOD for typing

--- packages/unmock-core/src/service/constants.ts
+++ packages/unmock-core/src/service/constants.ts
@@ -1,8 +1,6 @@
 import XRegExp from "xregexp";
-import { HTTPMethod } from "./interfaces";
 
 export const DEFAULT_ENDPOINT = "**";
-export const DEFAULT_REST_METHOD: HTTPMethod = "all";
 export const OAS_PATH_PARAM_REGEXP = XRegExp("{([^}]+)}");
 export const OAS_PATH_PARAMS_KW = "path";
 export const UNMOCK_PATH_REGEX_KW = "x-unmock-path-regex";
