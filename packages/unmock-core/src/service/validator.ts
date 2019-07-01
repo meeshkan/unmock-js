f4e15a34219d8626a33b5e74fd350aa75ca3d76e
Cain (Meeshkan) <idan@meeshkan.com>
2019-07-01 15:25:34 +0300

    Adds comments and simplifies

--- packages/unmock-core/src/service/validator.ts
+++ packages/unmock-core/src/service/validator.ts
@@ -12,40 +12,66 @@ import {
 } from "./interfaces";
 
 export class StateRequestValidator {
-  public static findPathInObject(
-    path: any,
-    obj: any,
-  ): { [pathKey: string]: any | undefined } | undefined {
+  /**
+   * Given a state request, finds the matching objects
+   * within the schema that apply to the request. These
+   * are fetched so that one can use the spread operator
+   * to overwrite the default schema.
+   * The state items are matched by identifying all corresponding
+   * keys nested in the path leading to them.
+   *
+   * @param statePath A state being set in a service (or recursively-called, hence `any`)
+   * @param serviceSchema The top-level (or recursely-called, hence `any`) schema for the service
+   * @returns An object with string-keys leading to the location of matching state for each variable,
+   *          with value being either the Schema defined for that variable, or undefined.
+   */
+  public static findStatePathInService(
+    statePath: any,
+    serviceSchema: any,
+  ): { [pathKey: string]: any | undefined } {
     const matches: { [key: string]: any } = {};
 
-    for (const key of Object.keys(path)) {
-      if (["string", "number", "boolean"].includes(typeof path[key])) {
-        if (isSchema(obj[key])) {
-          matches[key] = obj[key];
-        } else {
-          const nestedMatch = this.matchNested(obj, key);
-          matches[key] = nestedMatch;
-        }
-        continue;
-      }
-
-      if (typeof path[key] === "object" && Object.keys(path[key]).length > 0) {
+    for (const key of Object.keys(statePath)) {
+      // Traverse along the different paths in the state
+      if (["string", "number", "boolean"].includes(typeof statePath[key])) {
+        // The value of the current state key is concrete
+        // If the service schema contains a matching value (schema), store that one
+        // Otherwise recursively look for matching key in nested schema.
+        matches[key] = isSchema(serviceSchema[key])
+          ? serviceSchema[key]
+          : this.matchNested(serviceSchema, key);
+      } else if (
+        typeof statePath[key] === "object" &&
+        Object.keys(statePath[key]).length > 0
+      ) {
+        // The value of the current state key is a valid object (read: path to definition)
+        // If the service schema ends with that key, or does not define more paths - set as undefined
+        // Otherwise continue traversing both the state and the service schema
         matches[key] =
-          typeof obj[key] !== "object" || Object.keys(obj[key]).length === 0
+          typeof serviceSchema[key] !== "object" ||
+          Object.keys(serviceSchema[key]).length === 0
             ? undefined
-            : this.findPathInObject(path[key], obj[key]);
-        continue;
+            : this.findStatePathInService(statePath[key], serviceSchema[key]);
+      } else {
+        // None of the above - this hints at malformed state (i.e. `{ someKey: undefined }`)
+        throw new Error(
+          `${statePath[key]} (object '${JSON.stringify(
+            statePath,
+          )}' with key '${key}') is not an object, string, number or boolean!`,
+        );
       }
-
-      throw new Error(
-        `${path[key]} (object '${JSON.stringify(
-          path,
-        )}' with key '${key}') is not an object, string, number or boolean!`,
-      );
     }
     return matches;
   }
 
+  /**
+   * Given a state and an operation, returns all the valid responses that
+   * match the given state.
+   * First-level filtering is done via $code (status code) if it exists,
+   * otherwise via matching the parameters set in `state`.
+   * @param operation
+   * @param state
+   */
   public static getValidResponsesForOperationWithState(
     operation: Operation,
     state: UnmockServiceState,
@@ -63,25 +89,27 @@ export class StateRequestValidator {
       delete state.$code; // TODO: Remove other top-level DSL elements...
       const content = response.content[Object.keys(response.content)[0]];
       return {
-        [statusCode]: this.verifyResponseWithState(content, state),
+        [statusCode]: this.getUpdatedStateFromContent(content, state),
       };
     }
     throw new Error(`Not implemented yet for all paths`);
   }
 
-  public static verifyResponseWithState(
+  /**
+   * Given a media type, return mapping between state, service and response.
+   * @param content
+   * @param state
+   */
+  public static getUpdatedStateFromContent(
     content: MediaType,
     state: UnmockServiceState,
   ) {
     // Employ DFS to iterate over state and see if it matches in response...
     // (For now, ignore $DSL notation and types)
-    const responseModsForState = this.findPathInObject(
+    const responseModsForState = this.findStatePathInService(
       state,
       content,
     ) as Record<string, Schema>;
-    if (responseModsForState === undefined) {
-      throw new Error();
-    }
     // TODO: Ignore DSL/convert elements
     // verify none of the elements are `undefined`, throws if some are missing
     const { missingParam } = this.DFSVerifyNoneAreUndefined(
@@ -93,6 +121,15 @@ export class StateRequestValidator {
     return responseModsForState;
   }
 
+  /**
+   * Recursively iterates over given `obj` and verifies all values are
+   * properly defined.
+   * @param obj Object to iterate over
+   * @param prevPath (Used internaly) tracked the path to the key that might
+   *                 be undefined.
+   * @return A dictionary, potentially with `missingParam` holding the path
+   *         to a key that holds `undefined` as value.
+   */
   private static DFSVerifyNoneAreUndefined(
     obj: any,
     prevPath: string[] = [],
@@ -109,6 +146,14 @@ export class StateRequestValidator {
     return {};
   }
 
+  /**
+   * Helper method to `findStatePathInService`;
+   * Helps find given `pathKey` in `obj` in a recursive manner.
+   * @param obj
+   * @param pathKey
+   * @returns An object leading up to the requested key and it's value in `obj`
+   *          or `undefined`.
+   */
   private static matchNested(obj: any, pathKey: string) {
     const foundPath: { [key: string]: any } = {};
 
