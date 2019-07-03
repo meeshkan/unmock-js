9f7cdf75123cd7086b994588dc1100d0cdf94833
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-07-03 09:56:17 +0300

    Track client requests to emit errors when things go wrong (#62)
    * Add tracking for client requests, owes a lot to yesno.
    
    * Simplify.
    
    * Do not track req options.
    
    * Simplify a lot, hopefully works.
    
    * Fix typo.
    
    * Clean-up.
    
    * Add docs.
    
    * Update package-lock.json.
    
    * Slightly better error messages.

--- packages/unmock-node/src/__tests__/backend/index.test.ts
+++ packages/unmock-node/src/__tests__/backend/index.test.ts
@@ -8,18 +8,27 @@ const servicesDirectory = path.join(__dirname, "..", "loaders", "resources");
 describe("Node.js interceptor", () => {
   describe("with petstore in place", () => {
     let nodeInterceptor: NodeBackend;
-    beforeEach(() => {
+    beforeAll(() => {
       nodeInterceptor = new NodeBackend({ servicesDirectory });
       const unmockOptions = new UnmockOptions();
       nodeInterceptor.initialize(unmockOptions);
     });
-    afterEach(() => {
+    afterAll(() => {
       nodeInterceptor.reset();
     });
-    test("gets successful response", async () => {
+    test("gets successful response for valid request", async () => {
       const response = await axios("http://petstore.swagger.io/v1/pets");
       expect(response.status).toBe(200);
       expect(response.data).toBeDefined();
+    });
+    test("emits an error for unknown url", async () => {
+      try {
+        await axios("http://example.org");
+      } catch (err) {
+        expect(err.message).toBe("No matching template found");
+        return;
+      }
+      throw new Error("Should not get here");
     });
   });
 });
--- packages/unmock-node/src/backend/index.ts
+++ packages/unmock-node/src/backend/index.ts
@@ -1,5 +1,13 @@
-import { IncomingMessage, ServerResponse } from "http";
+import debug from "debug";
+import {
+  ClientRequest,
+  IncomingMessage,
+  RequestOptions,
+  ServerResponse,
+} from "http";
+import _ from "lodash";
 import Mitm from "mitm";
+import net from "net";
 import {
   CreateResponse,
   IBackend,
@@ -8,9 +16,11 @@ import {
   responseCreatorFactory,
   UnmockOptions,
 } from "unmock-core";
+import { v4 as uuidv4 } from "uuid";
 import { FsServiceDefLoader } from "../loaders/fs-service-def-loader";
 import { serializeRequest } from "../serialize";
-import * as constants from "./constants";
+
+const debugLog = debug("unmock:node");
 
 const respondFromSerializedResponse = (
   serializedResponse: ISerializedResponse,
@@ -24,23 +34,24 @@ async function handleRequestAndResponse(
   createResponse: CreateResponse,
   req: IncomingMessage,
   res: ServerResponse,
+  clientRequest: ClientRequest, // For emitting errors
 ) {
   try {
     const serializedRequest: ISerializedRequest = await serializeRequest(req);
+    debugLog("Serialized request", JSON.stringify(serializedRequest));
     const serializedResponse: ISerializedResponse | undefined = createResponse(
       serializedRequest,
     );
+
     if (serializedResponse === undefined) {
-      throw Error(constants.MESSAGE_FOR_MISSING_MOCK);
+      // TODO Handle this properly
+      debugLog("No match found, emitting error");
+      clientRequest.emit("error", Error("No matching template found"));
+      return;
     }
     respondFromSerializedResponse(serializedResponse, res);
   } catch (err) {
-    // TODO Emit an error in the corresponding client request instead?
-    const errorResponse: ISerializedResponse = {
-      body: err.message,
-      statusCode: constants.STATUS_CODE_FOR_ERROR,
-    };
-    respondFromSerializedResponse(errorResponse, res);
+    clientRequest.emit("error", Error(`unmock error: ${err.message}`));
   }
 }
 
@@ -48,34 +59,123 @@ export interface INodeBackendOptions {
   servicesDirectory?: string;
 }
 
-const nodeBackendDefaultOptions: INodeBackendOptions = {};
+interface IBypassableSocket extends net.Socket {
+  bypass: () => void;
+}
 
-let mitm: any;
+const nodeBackendDefaultOptions: INodeBackendOptions = {};
+const UNMOCK_INTERNAL_HTTP_HEADER = "x-unmock-req-id";
+
 export default class NodeBackend implements IBackend {
   private readonly config: INodeBackendOptions;
+  private clientRequests: {
+    [requestId: string]: {
+      clientRequest: ClientRequest;
+    };
+  } = {};
+  private origOnSocket?: (socket: net.Socket) => void;
+  private mitm: any;
+
   public constructor(config?: INodeBackendOptions) {
     this.config = { ...nodeBackendDefaultOptions, ...config };
   }
 
   public initialize(options: UnmockOptions) {
-    mitm = Mitm();
-    mitm.on("connect", (socket: any, opts: any) => {
-      if (options.isWhitelisted(opts.host)) {
-        socket.bypass();
-      }
-    });
+    if (this.mitm !== undefined) {
+      this.reset();
+    }
+    this.mitm = Mitm();
+
+    const self = this;
+    this.origOnSocket = ClientRequest.prototype.onSocket;
+
+    /**
+     * When a socket is assigned to a client request, create an internal ID
+     * and add the ID in the request header. Thereby we can map
+     * the server-side incoming request to the corresponding
+     * client request and emit errors properly.
+     * Borrowed from https://github.com/FormidableLabs/yesno.
+     */
+    ClientRequest.prototype.onSocket = _.flowRight(
+      this.origOnSocket,
+      function(
+        this: ClientRequest,
+        socket: IBypassableSocket,
+      ): IBypassableSocket {
+        const requestId = uuidv4();
+        debugLog(
+          `New socket assigned to client request, assigned ID: ${requestId}`,
+        );
+        self.clientRequests[requestId] = { clientRequest: this };
+        this.setHeader(UNMOCK_INTERNAL_HTTP_HEADER, requestId);
+        return socket;
+      },
+    );
+
+    // Client-side socket connect, use to bypass connections
+    this.mitm.on("connect", (socket: IBypassableSocket, opts: RequestOptions) =>
+      this.mitmOnConnect.call(this, options, socket, opts),
+    );
+
     // Prepare the request-response mapping by bootstrapping all dependencies here
     const serviceDefLoader = new FsServiceDefLoader({
       servicesDir: this.config.servicesDirectory,
     });
-    const createResponse = responseCreatorFactory({ serviceDefLoader });
-    mitm.on("request", (req: IncomingMessage, res: ServerResponse) => {
-      handleRequestAndResponse(createResponse, req, res);
+    const createResponse = responseCreatorFactory({
+      serviceDefLoader,
     });
+
+    this.mitm.on("request", (req: IncomingMessage, res: ServerResponse) =>
+      this.mitmOnRequest.call(this, createResponse, req, res),
+    );
   }
+
   public reset() {
-    if (mitm) {
-      mitm.disable();
+    if (this.mitm) {
+      this.mitm.disable();
+      this.mitm = undefined;
+    }
+    if (this.origOnSocket) {
+      ClientRequest.prototype.onSocket = this.origOnSocket;
+      this.origOnSocket = undefined;
+    }
+    this.clientRequests = {};
+  }
+
+  private extractTrackedClientRequest(
+    incomingMessage: IncomingMessage,
+  ): ClientRequest {
+    const { [UNMOCK_INTERNAL_HTTP_HEADER]: reqId } = incomingMessage.headers;
+    debugLog(
+      `Intercepted incoming request with ID ${reqId}, matching to existing IDs:`,
+      Object.keys(this.clientRequests),
+    );
+    if (typeof reqId !== "string") {
+      throw Error("Expected to get a non empty request ID");
+    }
+    const clientRequest = this.clientRequests[reqId].clientRequest;
+    if (clientRequest === undefined) {
+      throw Error(`Expected to find a client request for request ID ${reqId}`);
+    }
+    return clientRequest;
+  }
+
+  private mitmOnRequest(
+    createResponse: CreateResponse,
+    req: IncomingMessage,
+    res: ServerResponse,
+  ) {
+    const clientRequest = this.extractTrackedClientRequest(req);
+    handleRequestAndResponse(createResponse, req, res, clientRequest);
+  }
+
+  private mitmOnConnect(
+    unmockOptions: UnmockOptions,
+    socket: IBypassableSocket,
+    opts: RequestOptions,
+  ) {
+    if (unmockOptions.isWhitelisted(opts.host || "")) {
+      socket.bypass();
     }
   }
 }
