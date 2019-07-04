3ec92684babb5339e58f8116bb00f4744eb9cb55
Cain (Meeshkan) <idan@meeshkan.com>
2019-07-04 15:22:54 +0300

    Fixes node basic tests

--- packages/unmock-node/src/__tests__/backend/index.test.ts
+++ packages/unmock-node/src/__tests__/backend/index.test.ts
@@ -8,19 +8,23 @@ const servicesDirectory = path.join(__dirname, "..", "loaders", "resources");
 describe("Node.js interceptor", () => {
   describe("with petstore in place", () => {
     let nodeInterceptor: NodeBackend;
+
     beforeAll(() => {
       nodeInterceptor = new NodeBackend({ servicesDirectory });
       const unmockOptions = new UnmockOptions();
       nodeInterceptor.initialize(unmockOptions);
     });
+
     afterAll(() => {
       nodeInterceptor.reset();
     });
+
     test("gets successful response for valid request", async () => {
       const response = await axios("http://petstore.swagger.io/v1/pets");
       expect(response.status).toBe(200);
       expect(response.data).toBeDefined();
     });
+
     test("emits an error for unknown url", async () => {
       try {
         await axios("http://example.org");
@@ -30,5 +34,7 @@ describe("Node.js interceptor", () => {
       }
       throw new Error("Should not get here");
     });
+
+    test("modifies state as requested", async () => {});
   });
 });
