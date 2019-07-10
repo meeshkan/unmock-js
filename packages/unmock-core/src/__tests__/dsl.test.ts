055d8529e603a22a2f3ea1bb2475475d4f0cffa7
Cain (Meeshkan) <idan@meeshkan.com>
2019-07-10 12:48:20 +0300

    Adds tests for

---
+++ packages/unmock-core/src/__tests__/dsl.test.ts
@@ -0,0 +1,111 @@
+import { DSL } from "../service/dsl";
+import { codeToMedia } from "../service/interfaces";
+
+// tslint:disable: object-literal-key-quotes
+
+const responsesWithoutProperties: codeToMedia = {
+  200: {
+    "application/json": {},
+  },
+};
+
+const responsesWithProperties: codeToMedia = {
+  200: {
+    "application/json": {
+      properties: {
+        foo: {
+          type: "integer",
+        },
+      },
+    },
+  },
+};
+
+describe("Resolves top level DSL to OAS", () => {
+  it("Returns undefined with undefined input", () => {
+    expect(DSL.translateTopLevelToOAS({}, undefined)).toBeUndefined();
+  });
+
+  describe("Handles $times correctly", () => {
+    it("Throws on invalid $times (0)", () => {
+      const translated = () =>
+        DSL.translateTopLevelToOAS({ $times: 0.3 }, responsesWithoutProperties);
+      expect(translated).toThrow("Can't set response $times to 0.3"); // 0.3 gets rounded to 0 and throws
+    });
+
+    it("Throws on invalid $times (negative)", () => {
+      const translated = () =>
+        DSL.translateTopLevelToOAS({ $times: -1 }, responsesWithoutProperties);
+      expect(translated).toThrow("Can't set response $times to -1");
+    });
+
+    it("Adds properties when missing", () => {
+      const translated = DSL.translateTopLevelToOAS(
+        { $times: 1 },
+        responsesWithoutProperties,
+      );
+      expect(translated).toEqual({
+        200: {
+          "application/json": {
+            properties: { "x-unmock-times": { type: "unmock", default: 1 } },
+          },
+        },
+      });
+    });
+
+    it("Adds to properties when it exists", () => {
+      const translated = DSL.translateTopLevelToOAS(
+        { $times: 2 },
+        responsesWithProperties,
+      );
+      expect(translated).toEqual({
+        200: {
+          "application/json": {
+            properties: {
+              foo: { type: "integer" },
+              "x-unmock-times": { type: "unmock", default: 2 },
+            },
+          },
+        },
+      });
+    });
+  });
+});
+
+describe("Acts on top level DSL in OAS", () => {
+  describe("Acts on $times correctly", () => {
+    let states: codeToMedia;
+    beforeEach(
+      () =>
+        (states = {
+          200: {
+            "application/json": {
+              properties: {
+                "x-unmock-times": { type: "unmock", default: 2 },
+              },
+            },
+          },
+        }),
+    );
+
+    it("Removes $times in returned copy", () => {
+      const copy = DSL.actTopLevelFromOAS(states);
+      // removes properties as nothing's there anymore
+      expect(copy).toEqual({});
+    });
+
+    it("Decreases $times in original states", () => {
+      DSL.actTopLevelFromOAS(states);
+      expect(states["200"]["application/json"].properties).toEqual({
+        "x-unmock-times": { type: "unmock", default: 1 },
+      });
+    });
+
+    it("Removes $times from original states", () => {
+      DSL.actTopLevelFromOAS(states);
+      DSL.actTopLevelFromOAS(states);
+      DSL.actTopLevelFromOAS(states);
+      expect(states["200"]["application/json"]).toBeUndefined(); // media type is removed if its now empty
+    });
+  });
+});
