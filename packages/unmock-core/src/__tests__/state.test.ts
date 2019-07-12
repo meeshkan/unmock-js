509961d9b49b5a508298ccf1b42292bbe798c3f0
Cain (Meeshkan) <idan@meeshkan.com>
2019-07-12 12:48:25 +0300

    Adds shortcuts in state.test.ts

--- packages/unmock-core/src/__tests__/state.test.ts
+++ packages/unmock-core/src/__tests__/state.test.ts
@@ -1,4 +1,9 @@
-import defMiddleware from "../service/state/middleware";
+import {
+  ExtendedHTTPMethod,
+  HTTPMethod,
+  IStateInputGenerator,
+} from "../service/interfaces";
+import defMiddleware, { textMW } from "../service/state/middleware";
 import { State } from "../service/state/state";
 
 const fullSchema = {
@@ -37,11 +42,7 @@ const fullSchema = {
               },
               "text/plain": {
                 schema: {
-                  properties: {
-                    notId: {
-                      type: "string",
-                    },
-                  },
+                  type: "string",
                 },
               },
             },
@@ -66,72 +67,53 @@ const fullSchema = {
   },
 };
 
+const updateState = (
+  state: State,
+  method: ExtendedHTTPMethod,
+  endpoint: string,
+  newState: IStateInputGenerator,
+  schemaEndpoint: string,
+) =>
+  state.update({
+    stateInput: {
+      method,
+      endpoint,
+      newState,
+    },
+    serviceName: "foo",
+    paths: fullSchema.paths,
+    schemaEndpoint,
+  });
+
+const getState = (state: State, method: HTTPMethod, endpoint: string) =>
+  state.getState(method, endpoint, fullSchema.paths["/test/{test_id}"].get);
+
 describe("Test state management", () => {
   it("returns undefined when setting empty state", () => {
     const state = new State();
     expect(
-      state.update({
-        stateInput: {
-          method: "any",
-          endpoint: "**",
-          newState: defMiddleware(),
-        },
-        serviceName: "foo",
-        paths: fullSchema.paths,
-        schemaEndpoint: "**",
-      }),
+      updateState(state, "any", "**", defMiddleware(), "**"),
     ).toBeUndefined();
   });
 
   it("throws when setting state for non-existing method with generic endpoint", () => {
     const state = new State();
     expect(() =>
-      state.update({
-        stateInput: {
-          method: "post",
-          endpoint: "**",
-          newState: defMiddleware(),
-        },
-        serviceName: "foo",
-        paths: fullSchema.paths,
-        schemaEndpoint: "**",
-      }),
+      updateState(state, "post", "**", defMiddleware(), "**"),
     ).toThrow("Can't find any endpoints with method 'post'");
   });
 
   it("throws when setting state for non-existing method with specific, existing endpoint", () => {
     const state = new State();
     expect(() =>
-      state.update({
-        stateInput: {
-          method: "post",
-          endpoint: "/test/5",
-          newState: defMiddleware(),
-        },
-        serviceName: "foo",
-        paths: fullSchema.paths,
-        schemaEndpoint: "/test/{test_id}",
-      }),
+      updateState(state, "post", "/test/5", defMiddleware(), "/test/{test_id}"),
     ).toThrow("Can't find response for 'post /test/5'");
   });
 
   it("saves state from any endpoint and get method as expected", () => {
     const state = new State();
-    state.update({
-      stateInput: {
-        method: "get",
-        endpoint: "**",
-        newState: defMiddleware({ foo: { id: 5 } }),
-      },
-      serviceName: "foo",
-      paths: fullSchema.paths,
-      schemaEndpoint: "**",
-    });
-    const stateResult = state.getState(
-      "get",
-      "/",
-      fullSchema.paths["/test/{test_id}"].get,
-    );
+    updateState(state, "get", "**", defMiddleware({ foo: { id: 5 } }), "**");
+    const stateResult = getState(state, "get", "/");
     expect(stateResult).toEqual({
       200: {
         "application/json": {
@@ -151,18 +133,8 @@ describe("Test state management", () => {
 
   it("parses $times on specific endpoint as expected", () => {
     const state = new State();
-    state.update({
-      stateInput: {
-        method: "get",
-        endpoint: "**",
-        newState: defMiddleware({ id: 5, $times: 2 }),
-      },
-      serviceName: "foo",
-      paths: fullSchema.paths,
-      schemaEndpoint: "**",
-    });
-    const getRes = () =>
-      state.getState("get", "/", fullSchema.paths["/test/{test_id}"].get);
+    updateState(state, "get", "**", defMiddleware({ id: 5, $times: 2 }), "**");
+    const getRes = () => getState(state, "get", "/");
     expect(getRes()).toEqual({
       200: {
         "application/json": {
@@ -204,21 +176,8 @@ describe("Test state management", () => {
 
   it("saves state from any endpoint and any method as expected", () => {
     const state = new State();
-    state.update({
-      stateInput: {
-        method: "any",
-        endpoint: "**",
-        newState: defMiddleware({ id: 5 }),
-      },
-      serviceName: "foo",
-      paths: fullSchema.paths,
-      schemaEndpoint: "**",
-    });
-    const stateResult = state.getState(
-      "get",
-      "/",
-      fullSchema.paths["/test/{test_id}"].get,
-    );
+    updateState(state, "any", "**", defMiddleware({ id: 5 }), "**");
+    const stateResult = getState(state, "get", "/");
     expect(stateResult).toEqual({
       200: {
         "application/json": {
@@ -234,41 +193,22 @@ describe("Test state management", () => {
 
   it("spreads states from multiple paths correctly", () => {
     const state = new State();
-    state.update({
-      stateInput: {
-        method: "any",
-        endpoint: "**",
-        newState: defMiddleware({ id: 5 }),
-      },
-      serviceName: "foo",
-      paths: fullSchema.paths,
-      schemaEndpoint: "**",
-    });
-    state.update({
-      stateInput: {
-        method: "any",
-        endpoint: "/test/5",
-        newState: defMiddleware({ id: 3 }),
-      },
-      serviceName: "foo",
-      paths: fullSchema.paths,
-      schemaEndpoint: "/test/{test_id}",
-    });
-    state.update({
-      stateInput: {
-        method: "any",
-        endpoint: "/test/*",
-        newState: defMiddleware({ id: -1 }),
-      },
-      serviceName: "foo",
-      paths: fullSchema.paths,
-      schemaEndpoint: "/test/{test_id}",
-    });
-    const stateResult = state.getState(
-      "get",
+    updateState(state, "any", "**", defMiddleware({ id: 5 }), "**");
+    updateState(
+      state,
+      "any",
       "/test/5",
-      fullSchema.paths["/test/{test_id}"].get,
+      defMiddleware({ id: 3 }),
+      "/test/{test_id}",
     );
+    updateState(
+      state,
+      "any",
+      "/test/*",
+      defMiddleware({ id: -1 }),
+      "/test/{test_id}",
+    );
+    const stateResult = getState(state, "get", "/test/5");
     expect(stateResult).toEqual({
       200: {
         "application/json": {
@@ -284,21 +224,8 @@ describe("Test state management", () => {
 
   it("saves nested state correctly", () => {
     const state = new State();
-    state.update({
-      stateInput: {
-        method: "any",
-        endpoint: "**",
-        newState: defMiddleware({ foo: { id: 5 } }),
-      },
-      serviceName: "foo",
-      paths: fullSchema.paths,
-      schemaEndpoint: "**",
-    });
-    const stateResult = state.getState(
-      "get",
-      "/",
-      fullSchema.paths["/test/{test_id}"].get,
-    );
+    updateState(state, "any", "**", defMiddleware({ foo: { id: 5 } }), "**");
+    const stateResult = getState(state, "get", "/");
     expect(stateResult).toEqual({
       200: {
         "application/json": {
