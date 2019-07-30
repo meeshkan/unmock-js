03e772407307db98dde490dae1d9e70c9b07b6da
Cain (Meeshkan) <idan@meeshkan.com>
2019-07-30 12:21:18 +0300

    Adds test for concatenating whitelisted urls

--- packages/unmock-core/src/__tests__/corePackage.test.ts
+++ packages/unmock-core/src/__tests__/corePackage.test.ts
@@ -16,29 +16,40 @@ class TestBackend implements IBackend {
 }
 const backend = new TestBackend();
 
-describe("tests whitelist", () => {
-  test("test default whitelist", () => {
+describe("tests allowedHosts", () => {
+  test("defaults work as expected", () => {
     const pkg = new TestPackage(backend);
     expect(pkg.isWhitelisted("127.0.0.1")).toEqual(true);
     expect(pkg.isWhitelisted("127.0.2.1")).toEqual(false);
   });
 
-  test("test wildcard mid-string", () => {
+  test("with wildcard mid-string", () => {
     const pkg = new TestPackage(backend);
-    pkg.whitelist = ["12*.0.*.1"];
+    pkg.allowedHosts = ["12*.0.*.1"];
     expect(pkg.isWhitelisted("127.0.foobar.1")).toEqual(true);
     expect(pkg.isWhitelisted("127.0..1")).toEqual(true);
     expect(pkg.isWhitelisted("127.0.1")).toEqual(false);
     expect(pkg.isWhitelisted("12.5.0.1.2.3.4.1")).toEqual(true);
   });
 
-  test("test modifying whitelist post-initialization", () => {
+  test("modified post-initialization", () => {
     const pkg = new TestPackage(backend);
     expect(pkg.isWhitelisted("127.0.0.1")).toEqual(true);
     expect(pkg.isWhitelisted("127.0.2.1")).toEqual(false);
-    pkg.whitelist = ["*foo*bar*.com"];
+    pkg.allowedHosts = ["*foo*bar*.com"];
     expect(pkg.isWhitelisted("127.0.foobar.1")).toEqual(false);
     expect(pkg.isWhitelisted("127.0.0.1")).toEqual(false);
+    expect(pkg.isWhitelisted("foobar.com")).toEqual(true);
+    expect(pkg.isWhitelisted("https://foo.bar.com")).toEqual(true);
+  });
+
+  test("concatenated", () => {
+    const pkg = new TestPackage(backend);
+    expect(pkg.isWhitelisted("127.0.0.1")).toEqual(true);
+    expect(pkg.isWhitelisted("127.0.2.1")).toEqual(false);
+    pkg.allowedHosts = pkg.allowedHosts.concat(["*foo*bar*.com"]);
+    expect(pkg.isWhitelisted("127.0.foobar.1")).toEqual(false);
+    expect(pkg.isWhitelisted("127.0.0.1")).toEqual(true);
     expect(pkg.isWhitelisted("foobar.com")).toEqual(true);
     expect(pkg.isWhitelisted("https://foo.bar.com")).toEqual(true);
   });
