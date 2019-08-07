ca290529357f9ea9579b737e4f3af9efa0a62204
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-07 14:31:35 +0300

    Get rid of ts-jest in favor of babel.

--- jest.config.base.js
+++ jest.config.base.js
@@ -2,7 +2,6 @@ const { defaults } = require("jest-config");
 
 module.exports = {
   testEnvironment: "node",
-  preset: "ts-jest",
   testMatch: null,
   testRegex: "/__tests__/.*\\.test\\.(js|ts)$",
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
@@ -12,11 +11,5 @@ module.exports = {
       "<rootDir>/../../packages/$1/src$2"
   },
   setupFilesAfterEnv: ["<rootDir>/../../jest.setup.js"],
-  clearMocks: true,
-  globals: {
-    "ts-jest": {
-      tsConfig: "<rootDir>/src/__tests__/tsconfig.json",
-      diagnostics: false
-    }
-  }
+  clearMocks: true
 };
