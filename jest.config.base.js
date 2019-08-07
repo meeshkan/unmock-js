baee618207ef1c3c790959d3b05d453ba3717927
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-07 16:57:11 +0300

    Go back to using ts-jest to make coverage work.

--- jest.config.base.js
+++ jest.config.base.js
@@ -2,6 +2,7 @@ const { defaults } = require("jest-config");
 
 module.exports = {
   testEnvironment: "node",
+  preset: "ts-jest",
   testMatch: null,
   testRegex: "/__tests__/.*\\.test\\.(js|ts)$",
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
@@ -15,7 +16,13 @@ module.exports = {
       "/node_modules/(?!loas3)",
   ],
   collectCoverageFrom: [
-    "**/packages/*/**/*.ts",
+    "**/packages/*/src/**/*.ts",
     "!**/node_modules/**",
-  ]
+  ],
+  globals: {
+    "ts-jest": {
+      tsConfig: "<rootDir>/src/__tests__/tsconfig.json",
+      diagnostics: false
+    }
+  }
 };
