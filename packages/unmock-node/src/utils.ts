2c3d26f4e85b46dfaff51f794db5163e867793bf
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-09 14:31:29 +0300

    Also check process.cwd(), remove duplicates.

--- packages/unmock-node/src/utils.ts
+++ packages/unmock-node/src/utils.ts
@@ -1,6 +1,7 @@
 import appRootPath from "app-root-path";
 import debug from "debug";
 import fs from "fs";
+import { uniq } from "lodash";
 import path from "path";
 
 const debugLog = debug("unmock:node:utils");
@@ -15,8 +16,13 @@ export const resolveUnmockRootDirectory = (): string =>
  */
 export const resolveUnmockDirectories = (): string[] => {
   const defaultUnmockDirectories = [
-    appRootPath.resolve("node_modules/@unmock"),
     resolveUnmockRootDirectory(),
+    appRootPath.resolve("node_modules/@unmock"),
+    // Also search process cwd, helpful with `npm link` for example
+    // where app-root-path fails due to a symlink
+    // without using APP_ROOT_PATH=`pwd`
+    path.resolve(`${process.cwd()}/__unmock__`),
+    path.resolve(`${process.cwd()}/node_modules/@unmock`),
   ];
 
   const triedDirectories = process.env.UNMOCK_SERVICES_DIRECTORY
@@ -25,9 +31,11 @@ export const resolveUnmockDirectories = (): string[] => {
       )
     : defaultUnmockDirectories;
 
+  const uniqueTriedDirectories = uniq(triedDirectories);
+
   debugLog(`Checking for directories: ${triedDirectories}`);
 
-  return triedDirectories.filter(
+  return uniqueTriedDirectories.filter(
     (directory: string) =>
       fs.existsSync(directory) && fs.statSync(directory).isDirectory(),
   );
