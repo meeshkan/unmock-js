6996210993a7838a392c474325dac1daedfdf9f2
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-09 10:42:40 +0300

    Use Record.

--- packages/unmock-core/src/console/chalks.ts
+++ packages/unmock-core/src/console/chalks.ts
@@ -4,9 +4,10 @@ import { LogType } from "./types";
 const error = chalk.bold.red;
 const instruct = chalk.bold.magenta;
 
-export const Chalks: {
-  [key in LogType]: Chalk & { supportsColor: ColorSupport };
-} = {
+export const Chalks: Record<
+  LogType,
+  Chalk & { supportsColor: ColorSupport }
+> = {
   error,
   instruct,
 };
