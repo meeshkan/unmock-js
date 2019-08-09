0c668e2d7b2bceb6aed6d557bd804276d4466270
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-09 09:16:35 +0300

    Improve tests.

--- packages/unmock-core/src/__tests__/console.test.ts
+++ packages/unmock-core/src/__tests__/console.test.ts
@@ -1,5 +1,5 @@
 import { Writable } from "stream";
-import unmockConsole, { CustomConsole } from "../console";
+import { CustomConsole } from "../console";
 
 class ArrayWritable extends Writable {
   public readonly written: string[] = [];
@@ -7,30 +7,34 @@ class ArrayWritable extends Writable {
     super(options);
   }
 
-  protected _write(chunk: any, _: string, callback: any) {
+  public _write(chunk: any, _: string, callback: any) {
     this.written.push(chunk.toString());
     return callback();
   }
 }
 
-describe("Custom console", () => {
-  it("writes to given log stream", () => {
-    const logStream = new ArrayWritable();
-    const errorStream = new ArrayWritable();
-    const customConsole = new CustomConsole(logStream, errorStream);
+const getConsole = () => {
+  const stdout = new ArrayWritable();
+  const stderr = new ArrayWritable();
+  const customConsole = new CustomConsole(stdout, stderr);
+  return { stdout, stderr, customConsole };
+};
 
-    customConsole.instruct("Log message");
-    expect(logStream.written.length).toBe(1);
-    expect(logStream.written[0]).toContain("Log message");
+describe("Custom console", () => {
+  const logMessage = "Log message";
+  it("writes instructions", () => {
+    const { stdout, customConsole } = getConsole();
+    customConsole.instruct(logMessage);
+    expect(stdout.written.length).toBe(1);
+    expect(stdout.written[0]).toContain(logMessage);
+    expect(stdout.written[0]).not.toEqual(logMessage); // Formatting was done
   });
 
-  it("writes to given log stream", () => {
-    const logStream = new ArrayWritable();
-    const errorStream = new ArrayWritable();
-    const customConsole = new CustomConsole(logStream, errorStream);
+  it("writes logs", () => {
+    const { stdout, customConsole } = getConsole();
 
     customConsole.log("Log message");
-    expect(logStream.written.length).toBe(1);
-    expect(logStream.written[0]).toBe("Log message");
+    expect(stdout.written.length).toBe(1);
+    expect(stdout.written[0]).toBe("Log message");
   });
 });
