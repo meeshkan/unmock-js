3887641b5eb4eac871ee49c745e985a7969d3be1
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-19 14:34:01 +0300

    Add more clean-up.

--- packages/unmock-core/src/service/spy/index.ts
+++ packages/unmock-core/src/service/spy/index.ts
@@ -8,33 +8,69 @@ export type RequestResponseSpy = SinonSpy<
 >;
 
 export interface ISpyable {
+  /**
+   * Keep track of calls, added via the `notify` method
+   */
   spy: RequestResponseSpy;
+  /**
+   * Add new call to the spy object
+   * @param req Request
+   * @param res Response
+   */
   notify(req: ISerializedRequest, res: ISerializedResponse): void;
+  reset(): void;
 }
 
-class SpyContainer {
-  public readonly spy: RequestResponseSpy;
-  private response?: ISerializedResponse;
+export type RequestResponseSpyNotifier = SpyContainer<
+  ISerializedRequest,
+  ISerializedResponse
+>;
+
+/**
+ * Container for adding spy calls with `notify` method.
+ */
+class SpyContainer<TArg = any, TReturnValue = any> {
+  public readonly spy: SinonSpy<[TArg], TReturnValue>;
+  private returnValue?: TReturnValue;
   constructor() {
     // @ts-ignore
-    this.spy = sinonSpy(this, "gen");
+    this.spy = sinonSpy(this, "spiedFn");
   }
 
-  public notify(sreq: ISerializedRequest, sres: ISerializedResponse) {
+  public notify(targs: TArg, tret: TReturnValue) {
     // Ugly hack to predefine what the spied function should return
-    this.response = sres;
-    this.gen(sreq);
-    this.response = undefined;
+    this.returnValue = tret;
+    this.spiedFn(targs);
+    this.returnValue = undefined;
   }
 
-  private gen(_: ISerializedRequest): ISerializedResponse {
-    return this.response!;
+  public reset(): void {
+    this.spy.resetHistory();
+  }
+
+  private spiedFn(_: TArg): TReturnValue {
+    return this.returnValue!;
   }
 }
 
-export const createRequestResponseSpy = (): ISpyable => new SpyContainer();
+export const createRequestResponseSpy = (): ISpyable =>
+  new SpyContainer<ISerializedRequest, ISerializedResponse>();
 
 export const attach = (service: IService): IService & ISpyable => {
   const spyable = new SpyContainer();
   return Object.assign(service, spyable);
 };
+/*
+export interface Service {
+  spy: Spy;
+  state: State;
+  generate(req: Request): Response;
+}
+ */
+
+/* type ServiceCore = {
+  generate(sreq: ISerializedRequest): ISerializedResponse;
+};
+
+export type Service = ServiceCore & Pick<ISpyable, "spy" | "reset">;
+ */
