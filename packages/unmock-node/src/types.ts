22bdfbbb4738a49231399e0aeb5aa54297f1b246
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-20 11:39:56 +0300

    Keep the old Request export as duplicate still to not break existing stuff.

--- packages/unmock-node/src/types.ts
+++ packages/unmock-node/src/types.ts
@@ -1,5 +1,6 @@
 // Types and aliases exported from unmock-node
 export {
+  ISerializedRequest as Request,
   ISerializedRequest as UnmockRequest,
   ISerializedResponse as UnmockResponse,
   States,
