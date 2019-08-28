80eb98c82f07057bb052337c1f16f376365ccbee
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-28 12:57:33 +0300

    More hacking on types.

--- packages/unmock-core/src/service/spy/decorate.ts
+++ packages/unmock-core/src/service/spy/decorate.ts
@@ -2,40 +2,6 @@ import { match } from "sinon";
 import { HTTPMethod } from "../../interfaces";
 import { SinonSpy, UnmockServiceSpy } from "./types";
 
-// Base helper methods used for higher-level methods
-const helperMethods = {
-  withMethod(this: SinonSpy, method: HTTPMethod) {
-    return this.withArgs(match({ method }));
-  },
-};
-
-type SpyHelperMethods = typeof helperMethods;
-
-const postMethods = {
-  postRequestBody(this: SinonSpy & SpyHelperMethods): any {
-    const spyWithPost = this.withMethod("post");
-    if (spyWithPost.callCount !== 1) {
-      throw Error(
-        `postRequestBody: Expected one post call, got ${spyWithPost.callCount}`,
-      );
-    }
-    return spyWithPost.firstCall.args[0].body;
-  },
-  postResponseBody(this: SinonSpy & SpyHelperMethods): any {
-    const spyWithPost = this.withMethod("post");
-    if (spyWithPost.callCount !== 1) {
-      throw Error(
-        `postRequestBody: Expected one post call, got ${spyWithPost.callCount}`,
-      );
-    }
-    return spyWithPost.firstCall.args[0].body;
-  },
-};
-
-const customSpyMethods = { ...helperMethods, ...postMethods };
-
-export type ISpyDecoration = typeof customSpyMethods;
-
 /**
  * Add custom methods to spy
  * @param spy Sinon spy (NOTE: modified in-place!)
@@ -43,5 +9,41 @@ export type ISpyDecoration = typeof customSpyMethods;
 const decorateSpy = (spy: SinonSpy): UnmockServiceSpy => {
   return Object.assign(spy, customSpyMethods);
 };
+
+// Base helper methods used for higher-level methods
+const helperMethods = {
+  withMethod(this: UnmockServiceSpy, method: HTTPMethod): UnmockServiceSpy {
+    return decorateSpy(this.withArgs(match({ method })));
+  },
+};
+
+export interface ISpyDecoration {
+  withMethod(method: HTTPMethod): UnmockServiceSpy;
+  postRequestBody(): any;
+  postResponseBody(): any;
+}
+
+const postMethods = {
+  postRequestBody(this: UnmockServiceSpy): any {
+    const spyWithPost = this.withMethod("post");
+    if (spyWithPost.callCount !== 1) {
+      throw Error(
+        `postRequestBody: Expected one post call, got ${spyWithPost.callCount}`,
+      );
+    }
+    return spyWithPost.firstCall.args[0].body;
+  },
+  postResponseBody(this: UnmockServiceSpy): any {
+    const spyWithPost = this.withMethod("post");
+    if (spyWithPost.callCount !== 1) {
+      throw Error(
+        `postRequestBody: Expected one post call, got ${spyWithPost.callCount}`,
+      );
+    }
+    return spyWithPost.firstCall.args[0].body;
+  },
+};
+
+const customSpyMethods: ISpyDecoration = { ...helperMethods, ...postMethods };
 
 export default decorateSpy;
