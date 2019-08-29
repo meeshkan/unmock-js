4232a3c60e9e7149a061843daec39f26c8061f11
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-29 12:29:05 +0300

    Add test for other exports.

--- packages/unmock/src/__tests__/index.test.ts
+++ packages/unmock/src/__tests__/index.test.ts
@@ -1,8 +1,11 @@
-import unmock from "../";
+import unmock, { dsl } from "../";
 
 describe("Top level export", () => {
   it("should export unmock object", () => {
     expect(unmock).toBeDefined();
     expect(unmock).toHaveProperty("on");
+  });
+  it("should export what unmock-core exports", () => {
+    expect(dsl).toBeDefined();
   });
 });
