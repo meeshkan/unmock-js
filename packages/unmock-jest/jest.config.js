37c214af1396bb156bb234df8d9bd571bf4b67fc
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-10 13:34:23 +0300

    Refactor for easier usage.

--- packages/unmock-jest/jest.config.js
+++ packages/unmock-jest/jest.config.js
@@ -1,8 +1,5 @@
 const config = require("../../jest.config.base");
 
 module.exports = Object.assign(Object.create(null), config, {
-  reporters: [
-    "default",
-    "/Users/kimmo/git/meeshkan/unmock-js/packages/unmock-jest/dist/reporter.js",
-  ],
+  reporters: ["default", "./reporter"],
 });
