bf6b339be274c936f8d622fc053c0b7c92daebf2
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-10 14:57:38 +0300

    Remove dummy test.

--- packages/unmock-jest/src/__tests__/reporter.test.ts
+++
@@ -1,7 +0,0 @@
-import UnmockJestTestReporter from "../reporter";
-
-describe("Unmock Jest test reporter", () => {
-  it("needs tests", () => {
-    expect(UnmockJestTestReporter).toBeDefined();
-  });
-});
