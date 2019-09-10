122822eb2a6ca116d1de01489c8319fefdca89c8
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-10 13:39:56 +0300

    Revert reporter.

--- packages/unmock-jest/jest.config.js
+++ packages/unmock-jest/jest.config.js
@@ -1,5 +1,3 @@
 const config = require("../../jest.config.base");
 
-module.exports = Object.assign(Object.create(null), config, {
-  reporters: ["default", "./reporter"],
-});
+module.exports = Object.assign(Object.create(null), config);
