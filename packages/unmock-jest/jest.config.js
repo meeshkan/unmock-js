0af31423efeb278a6c35cb00e251ae1276fa9740
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-10 13:29:54 +0300

    Get rid of test results processor for now.

--- packages/unmock-jest/jest.config.js
+++ packages/unmock-jest/jest.config.js
@@ -1,3 +1,8 @@
 const config = require("../../jest.config.base");
 
-module.exports = Object.assign(Object.create(null), config);
+module.exports = Object.assign(Object.create(null), config, {
+  reporters: [
+    "default",
+    "/Users/kimmo/git/meeshkan/unmock-js/packages/unmock-jest/dist/reporter.js",
+  ],
+});
