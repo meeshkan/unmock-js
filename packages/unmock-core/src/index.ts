ba708ea8b50ea7ee12718f6c10a0a717f3205b4f
Cain (Meeshkan) <idan@meeshkan.com>
2019-09-10 12:47:40 +0300

    Adds dynamic, basic nock-like syntax

--- packages/unmock-core/src/index.ts
+++ packages/unmock-core/src/index.ts
@@ -4,6 +4,7 @@ import NodeBackend from "./backend";
 import { ILogger, IUnmockOptions, IUnmockPackage } from "./interfaces";
 import WinstonLogger from "./loggers/winston-logger";
 import { AllowedHosts, BooleanSetting } from "./settings";
+import { Schema } from "./service/interfaces";
 
 export * from "./types";
 export { sinon };
@@ -56,15 +57,133 @@ export class UnmockPackage implements IUnmockPackage {
   }
 }
 
-const unmock: IUnmockPackage = new UnmockPackage(new NodeBackend(), {
+const bknd = new NodeBackend();
+const unmockPackage: IUnmockPackage = new UnmockPackage(bknd, {
   logger: new WinstonLogger(),
 });
 
-// const dynamicService = (foo: any) => {
-//   unmockPackage.services;
-// };
+const dynamicService = (baseUrl: string) => ({
+  get(endpoint: string) {
+    return new DynamicServiceSpec(dss =>
+      bknd.updateServices({
+        baseUrl,
+        method: "get",
+        endpoint,
+        statusCode: dss.statusCode,
+        response: dss.data,
+      }),
+    );
+  },
+  head(endpoint: string) {
+    return new DynamicServiceSpec(dss =>
+      bknd.updateServices({
+        baseUrl,
+        method: "get",
+        endpoint,
+        statusCode: dss.statusCode,
+        response: dss.data,
+      }),
+    );
+  },
+  post(endpoint: string) {
+    return new DynamicServiceSpec(dss =>
+      bknd.updateServices({
+        baseUrl,
+        method: "get",
+        endpoint,
+        statusCode: dss.statusCode,
+        response: dss.data,
+      }),
+    );
+  },
+  put(endpoint: string) {
+    return new DynamicServiceSpec(dss =>
+      bknd.updateServices({
+        baseUrl,
+        method: "get",
+        endpoint,
+        statusCode: dss.statusCode,
+        response: dss.data,
+      }),
+    );
+  },
+  patch(endpoint: string) {
+    return new DynamicServiceSpec(dss =>
+      bknd.updateServices({
+        baseUrl,
+        method: "get",
+        endpoint,
+        statusCode: dss.statusCode,
+        response: dss.data,
+      }),
+    );
+  },
+  delete(endpoint: string) {
+    return new DynamicServiceSpec(dss =>
+      bknd.updateServices({
+        baseUrl,
+        method: "get",
+        endpoint,
+        statusCode: dss.statusCode,
+        response: dss.data,
+      }),
+    );
+  },
+  options(endpoint: string) {
+    return new DynamicServiceSpec(dss =>
+      bknd.updateServices({
+        baseUrl,
+        method: "get",
+        endpoint,
+        statusCode: dss.statusCode,
+        response: dss.data,
+      }),
+    );
+  },
+  trace(endpoint: string) {
+    return new DynamicServiceSpec(dss =>
+      bknd.updateServices({
+        baseUrl,
+        method: "get",
+        endpoint,
+        statusCode: dss.statusCode,
+        response: dss.data,
+      }),
+    );
+  },
+});
 
-// const unmock = Object.assign(dynamicService, unmockPackage);
+// Placeholder for poet input type, to have
+// e.g. standard object => { type: "object", properties: { ... }}, number => { type: "number", const: ... }
+type InputToPoet = any;
+// tslint:disable-next-line: max-classes-per-file
+class DynamicServiceSpec {
+  public statusCode: number = 200;
+  public data: Schema = {};
+
+  constructor(private updater: (input: DynamicServiceSpec) => void) {}
+
+  public reply(statusCode: number, data?: InputToPoet): void;
+  public reply(data: InputToPoet): void;
+  public reply(maybeStatusCode: number | InputToPoet, maybeData?: InputToPoet) {
+    if (maybeData !== undefined) {
+      this.data = maybeData as Schema; // TODO: use poet to convert to JSON Schema?
+      this.statusCode = maybeStatusCode;
+    } else if (
+      typeof maybeStatusCode === "number" &&
+      maybeStatusCode >= 100 &&
+      maybeStatusCode < 599
+    ) {
+      // we assume it's a status code
+      this.statusCode = maybeStatusCode;
+    } else {
+      this.data = maybeStatusCode as Schema; // TODO: ditto
+    }
+    this.updater(this);
+  }
+}
+
+const unmock = Object.assign(unmockPackage, dynamicService); // Add the function call to the unmock object
 
 export type UnmockNode = typeof unmock;
 
