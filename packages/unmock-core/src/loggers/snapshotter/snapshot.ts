527ea18daf2f085c3c82c203134fde19d2ccadf1
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-12 10:25:33 +0300

    Create directory on snapshot start.

--- packages/unmock-core/src/loggers/snapshotter/snapshot.ts
+++ packages/unmock-core/src/loggers/snapshotter/snapshot.ts
@@ -1,22 +1,47 @@
 import debug from "debug";
 import * as expect from "expect";
+import * as fs from "fs";
 import { IFSSnapshotterOptions } from ".";
 import { IListenerInput } from "../../interfaces";
 
 const debugLog = debug("unmock-core:snapshotter");
 
+const MATCHER_PASS = { pass: true };
+
 export const unmockSnapshot = (options: IFSSnapshotterOptions) => {
   // TODO Ensure destination exists and is writable
+
+  const outputFolder = options.outputFolder;
+
+  const exists =
+    !fs.existsSync(outputFolder) && fs.lstatSync(outputFolder).isDirectory();
+
+  let canWrite = true;
+
+  if (!exists) {
+    try {
+      fs.mkdirSync(outputFolder);
+    } catch (err) {
+      canWrite = false;
+      throw Error(
+        `Failed creating directory: ${outputFolder}, err: ${err.message}`,
+      );
+    }
+  }
+
   return function(this: expect.MatcherState, obj: IListenerInput) {
+    if (!canWrite) {
+      debugLog(`Cannot write to ${outputFolder}, skipping snapshotting`);
+      return MATCHER_PASS;
+    }
     debugLog("Snapshotting:", {
       outputFolder: options.outputFolder,
       testPath: this.testPath,
       currentTestName: this.currentTestName,
       obj,
     });
-    return {
-      pass: true,
-    };
+
+    return MATCHER_PASS;
   };
 };
 
