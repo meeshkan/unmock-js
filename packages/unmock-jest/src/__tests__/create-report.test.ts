bbaa16146d9e1906aba13b18da0a66dd5255fd84
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-13 14:41:44 +0300

    Add some fake data.

--- packages/unmock-jest/src/__tests__/create-report.test.ts
+++ packages/unmock-jest/src/__tests__/create-report.test.ts
@@ -1,7 +1,23 @@
 import createReport from "../reporter/create-report";
 import { IJestData, IReportInput } from "../reporter/types";
 
-const aggregatedResult: jest.AggregatedResult = {};
+const aggregatedResult: jest.AggregatedResult = {
+  numFailedTests: 0,
+  numPassedTests: 0,
+  numPendingTestSuites: 0,
+  numRuntimeErrorTestSuites: 0,
+  numTodoTests: 0,
+  numTotalTests: 0,
+  numFailedTestSuites: 0,
+  numPassedTestSuites: 0,
+  numTotalTestSuites: 0,
+  numPendingTests: 0,
+  snapshot: {} as jest.SnapshotSummary, // Don't care about these
+  startTime: 0,
+  success: true,
+  wasInterrupted: false,
+  testResults: [],
+};
 
 const jestData: IJestData = {
   aggregatedResult,
