77777cc1f57760f59beb32b0191b9d0358c37c8b
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-13 14:52:57 +0300

    Add data for testing.

--- packages/unmock-jest/src/__tests__/create-report.test.ts
+++ packages/unmock-jest/src/__tests__/create-report.test.ts
@@ -1,29 +1,5 @@
 import createReport from "../reporter/create-report";
-import { IJestData, IReportInput } from "../reporter/types";
-
-const aggregatedResult: jest.AggregatedResult = {
-  numFailedTests: 0,
-  numPassedTests: 0,
-  numPendingTestSuites: 0,
-  numRuntimeErrorTestSuites: 0,
-  numTodoTests: 0,
-  numTotalTests: 0,
-  numFailedTestSuites: 0,
-  numPassedTestSuites: 0,
-  numTotalTestSuites: 0,
-  numPendingTests: 0,
-  snapshot: {} as jest.SnapshotSummary, // Don't care about these
-  startTime: 0,
-  success: true,
-  wasInterrupted: false,
-  testResults: [],
-};
-
-const jestData: IJestData = {
-  aggregatedResult,
-};
-
-const exampleInput: IReportInput = { jestData, snapshots: [] };
+import { exampleInput } from "./fake-data";
 
 describe("Report creator", () => {
   it("should create report for given input", () => {
