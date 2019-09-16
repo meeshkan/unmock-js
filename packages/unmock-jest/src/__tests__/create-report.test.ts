dbd0274526d2e8a5edaab61831e76502fd79e646
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-16 14:58:46 +0300

    Refactor tests a bit.

--- packages/unmock-jest/src/__tests__/create-report.test.ts
+++ packages/unmock-jest/src/__tests__/create-report.test.ts
@@ -3,11 +3,12 @@ import "@testing-library/jest-dom/extend-expect";
 import { JSDOM } from "jsdom";
 import createReport from "../reporter/create-report";
 import { exampleInput } from "./fake-data";
-describe("Report creator", () => {
-  it("should create report for given input", () => {
-    const report = createReport(exampleInput);
-    expect(report).toBeDefined();
-    const dom = new JSDOM(report);
+
+describe("Report creator for given data", () => {
+  const report = createReport(exampleInput);
+  expect(report).toBeDefined();
+  const dom = new JSDOM(report);
+  it("should have title", () => {
     const title = dom.window.document.querySelector("h1");
     expect(title).toBeInTheDocument();
   });
