c7a0508fad6ebab8d547cf756451761ea017b76f
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-16 14:57:41 +0300

    Add jsdom and jest-jsdom for testing, use xml builder for creating the document.

--- packages/unmock-jest/src/__tests__/create-report.test.ts
+++ packages/unmock-jest/src/__tests__/create-report.test.ts
@@ -1,9 +1,14 @@
+// Matchers from jest-dom
+import "@testing-library/jest-dom/extend-expect";
+import { JSDOM } from "jsdom";
 import createReport from "../reporter/create-report";
 import { exampleInput } from "./fake-data";
-
 describe("Report creator", () => {
   it("should create report for given input", () => {
-    const created = createReport(exampleInput);
-    expect(created).toBeDefined();
+    const report = createReport(exampleInput);
+    expect(report).toBeDefined();
+    const dom = new JSDOM(report);
+    const title = dom.window.document.querySelector("h1");
+    expect(title).toBeInTheDocument();
   });
 });
--- packages/unmock-jest/src/reporter/create-report.ts
+++ packages/unmock-jest/src/reporter/create-report.ts
@@ -1,10 +1,25 @@
 // @ts-ignore
 import { Remarkable } from "remarkable";
 import { ISnapshot } from "unmock";
+import * as xmlBuilder from "xmlbuilder";
 import { IReportInput } from "./types";
 
 const md = new Remarkable();
-const buildBody2 = (_: IReportInput) => md.render("# Hello!");
+
+const createHtmlBase = () => {
+  const htmlBase = {
+    html: {
+      head: {
+        meta: { "@charset": "utf-8" },
+        title: { "#text": "Unmock report" },
+      },
+    },
+  };
+
+  return xmlBuilder.create(htmlBase);
+};
+
+const renderBodyHtml = (_: IReportInput) => md.render("# TITLE ");
 
 export const buildBody = (input: IReportInput): string => {
   const snapshots: ISnapshot[] = input.snapshots;
@@ -45,19 +60,10 @@ export const buildBody = (input: IReportInput): string => {
 };
 
 export const createReport = (input: IReportInput) => {
-  const body = buildBody2(input);
-  return `
-<!DOCTYPE html>
-  <html>
-    <head>
-      <title>
-        Unmock Report
-      </title>
-    </head>
-    <body>
-      ${body}
-    </body>
-  </html>`;
+  const htmlOutput = createHtmlBase();
+  const body = renderBodyHtml(input);
+  htmlOutput.ele("body").raw(body);
+  return htmlOutput.end({ pretty: true });
 };
 
 export default createReport;
