758d2197d92f2ea634e6723e5b1b200dbc456fb3
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-20 12:34:11 +0300

    Use CSS for switching between test suites, rewrite components with React. (#259)
    * Add test suites component.
    
    * Allow more dynamic css.
    
    * Add working CSS hack for showing only one test suite.
    
    * Add test suite component.
    
    * Add a comment.
    
    * Add test suite summary element.
    
    * Add test component.
    
    * Add styling.
    
    * Add failure messages.
    
    * Add calls to test.
    
    * Remove lots of xmlbuilder stuff.
    
    * Add primitive coloring to labels.
    
    * Style the label blocks.
    
    * Clean-up + remove lodash maps.

---
+++ packages/unmock-jest/src/reporter/components/test-suite.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { ITestSuite } from "../types";
+import Test from "./test";
+
+const Summary = ({ testSuite }: { testSuite: ITestSuite }) => {
+    const numPassingTests = testSuite.suiteResults.numPassingTests;
+    const numFailingTests = testSuite.suiteResults.numFailingTests;
+    const nSnapshots = testSuite.snapshots.length;
+    return (<div className="test-suite__title">
+        <div className="test-suite__title-filename">
+            {testSuite.testFilePath}
+        </div>
+        <div className="test-suite__title-summary">
+            {`Passing: ${numPassingTests}, failing: ${numFailingTests}, HTTP calls: ${nSnapshots}`}
+        </div>
+    </div>)
+};
+
+const TestSuite = ({ testSuite }: { testSuite: ITestSuite }) => {
+
+    const testElements = testSuite.suiteResults.testResults.map(
+        (assertionResult: jest.AssertionResult) => {
+            const snapshotsForTest = testSuite.snapshots.filter(
+                snapshot => snapshot.currentTestName === assertionResult.fullName,
+            );
+            return <Test assertionResult={assertionResult} snapshots={snapshotsForTest} key={assertionResult.fullName}/>
+        },
+  );
+
+    return (<div className="">
+            <Summary testSuite={testSuite}/>
+            {...testElements}
+    </div>)
+}
+
+export default TestSuite;
---
+++ packages/unmock-jest/src/reporter/components/test.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import stripAnsi from "strip-ansi";
+import { ISnapshot } from "unmock";
+import Calls from "./calls";
+
+const buildTestTitle = (assertionResult: jest.AssertionResult) =>
+  assertionResult.ancestorTitles
+    .map(ancestorTitle => `${ancestorTitle} > `)
+    .join(" ") + assertionResult.title;
+
+const TestTitle = ({ assertionResult }: { assertionResult: jest.AssertionResult}) => {
+    return (<div className="test__title">
+            {buildTestTitle(assertionResult)}
+    </div>);
+}
+
+const FailureMessage = ({ messages }: { messages: string[] }) => {
+    return (<div className={`test__failure-messages`}>
+                {`Failure message: ${messages.map((message) => stripAnsi(message)).join(", ")}`}
+            </div>) 
+}
+
+/**
+ * Build Test component
+ * @param assertionResult Jest results for the test
+ * @param snapshots All unmock snapshots for **this test**
+ */
+const Test = ({ assertionResult, snapshots }: { assertionResult: jest.AssertionResult, snapshots: ISnapshot[]}) => {
+
+    const failureMessages = assertionResult.failureMessages;
+
+    const statusClass =
+        failureMessages.length > 0
+        ? "test--failure"
+        : "test--success";
+
+    return (
+        <div className={`test ${statusClass}`}>
+            <TestTitle assertionResult={assertionResult} />
+            { failureMessages.length > 0 ?
+                <FailureMessage messages={failureMessages} />
+                : null }
+            <Calls assertionResult={assertionResult}  snapshots={snapshots}/>
+        </div>);
+};
+
+export default Test;
