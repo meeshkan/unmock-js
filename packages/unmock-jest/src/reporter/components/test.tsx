7b5242c4d273395832428ecfa65de255ee951f71
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-20 10:32:29 +0300

    Add calls to test.

--- packages/unmock-jest/src/reporter/components/test.tsx
+++ packages/unmock-jest/src/reporter/components/test.tsx
@@ -1,12 +1,18 @@
-import { map } from "lodash";
 import * as React from "react";
 import stripAnsi from "strip-ansi";
 import { ISnapshot } from "unmock";
+import Calls from "./calls";
 
 const buildTestTitle = (assertionResult: jest.AssertionResult) =>
   assertionResult.ancestorTitles
     .map(ancestorTitle => `${ancestorTitle} > `)
     .join(" ") + assertionResult.title;
+
+const TestTitle = ({ assertionResult }: { assertionResult: jest.AssertionResult}) => {
+    return (<div className="test__title">
+            {buildTestTitle(assertionResult)}
+    </div>);
+}
 
 const FailureMessage = ({ messages }: { messages: string[] }) => {
     return (<div className={`test__failure-messages`}>
@@ -23,14 +29,14 @@ const Test = ({ assertionResult, snapshots }: { assertionResult: jest.AssertionR
         ? "test--failure"
         : "test--success";
 
-    return (<div className={`test ${statusClass}`}>
-        <div className="test__title">
-            {buildTestTitle(assertionResult)}
-        </div>
-        { failureMessages.length > 0 ? 
-            <FailureMessage messages={failureMessages} />
-            : null }
-    </div>);
+    return (
+        <div className={`test ${statusClass}`}>
+            <TestTitle assertionResult={assertionResult} />
+            { failureMessages.length > 0 ?
+                <FailureMessage messages={failureMessages} />
+                : null }
+            <Calls assertionResult={assertionResult}  snapshots={snapshots}/>
+        </div>);
 };
 
 export default Test;
