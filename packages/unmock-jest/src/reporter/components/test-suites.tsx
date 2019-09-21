80b3617a1ee9338a26ea30fb9d4873031de6eb07
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-21 16:05:42 +0300

    Clean-up.

--- packages/unmock-jest/src/reporter/components/test-suites.tsx
+++ packages/unmock-jest/src/reporter/components/test-suites.tsx
@@ -4,7 +4,7 @@ import { ITestSuite } from "../types";
 import TestSuite from "./test-suite";
 
 // Replace file path separator chars with a dash
-export const testFileToId = (file: string) => file.replace(/(\/|\\(\\)?|:)/g, "-");  // TODO Use path.sep?
+export const testFileToId = (file: string) => file.replace(/(\/|\\(\\)?|:)/g, "-");
 
 /**
  * Not actually a React component but something that returns dynamically generated CSS and a React component
