e19fdf7473d48ba7c7238ba7e4f35f125e83e3d0
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-27 14:06:50 +0300

    Prepare for adding more tests.

--- packages/unmock-jest/src/__tests__/components/test-suites.test.tsx
+++ packages/unmock-jest/src/__tests__/components/test-suites.test.tsx
@@ -1,4 +1,15 @@
-import { testFileToId } from "../../reporter/components/test-suites";
+import TestSuites, { testFileToId } from "../../reporter/components/test-suites";
+import { ITestSuite } from "../../reporter/types";
+import { toTestSuites } from "../../reporter/utils";
+import { readJson } from "../utils";
+
+const jestResults = readJson("resources/jest-results.json");
+const unmockSnapshots = readJson("resources/unmock-snapshots.json");
+
+const input = {
+    jestData: { aggregatedResult: jestResults },
+    snapshots: unmockSnapshots,
+};
 
 describe("Test suites component", () => {
     describe("turning file path into id", () => {
@@ -7,6 +18,14 @@ describe("Test suites component", () => {
         });
         it("should change unix path to proper id", () => {
             expect(testFileToId("/unix/path/dir/file.txt")).toBe("-unix-path-dir-file-txt");
+        });
+    });
+
+    describe("rendering", () => {
+        const testSuites: ITestSuite[] = toTestSuites(input);
+        it("example test", () => {
+            const [css, TestSuitesComponent] = TestSuites({testSuites });
+            expect(testSuites).toHaveLength(2);
         });
     });
 });
