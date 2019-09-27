722573fe43d1442e646424f64c9ddb80013923f3
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-27 15:08:36 +0300

    Simplify.

--- packages/unmock-jest/src/__tests__/create-report.test.ts
+++ packages/unmock-jest/src/__tests__/create-report.test.ts
@@ -1,19 +1,10 @@
-// Matchers from jest-dom
 import "@testing-library/jest-dom/extend-expect";
-import * as fs from "fs";
 import { JSDOM } from "jsdom";
 import * as path from "path";
 import createReport from "../reporter/create-report";
 import { writeToDirectory } from "../reporter/write-report";
 import { exampleInput } from "./fake-data";
-
-const readJson = (pathToJson: string): any => {
-  const fullpath = path.isAbsolute(pathToJson)
-    ? pathToJson
-    : path.resolve(__dirname, pathToJson);
-  const fileContents = fs.readFileSync(fullpath, { encoding: "utf-8" });
-  return JSON.parse(fileContents);
-};
+import { readJson } from "./utils";
 
 const jestResults = readJson("resources/jest-results.json");
 const unmockSnapshots = readJson("resources/unmock-snapshots.json");
