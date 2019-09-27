a4359a4b1d1d1077fa472a0c80c37b059d706946
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-27 14:38:51 +0300

    Add JSDOM tests to test-suites.test.tsx.

--- packages/unmock-jest/src/__tests__/components/test-suites.test.tsx
+++ packages/unmock-jest/src/__tests__/components/test-suites.test.tsx
@@ -1,8 +1,11 @@
+import "@testing-library/jest-dom/extend-expect";
+import { JSDOM } from "jsdom";
+import * as React from "react";
+import * as ReactDomServer from "react-dom/server";
 import TestSuites, { testFileToId } from "../../reporter/components/test-suites";
 import { ITestSuite } from "../../reporter/types";
 import { toTestSuites } from "../../reporter/utils";
 import { readJson } from "../utils";
-
 const jestResults = readJson("resources/jest-results.json");
 const unmockSnapshots = readJson("resources/unmock-snapshots.json");
 
@@ -21,11 +24,30 @@ describe("Test suites component", () => {
         });
     });
 
-    describe("rendering", () => {
+    describe("rendered component", () => {
         const testSuites: ITestSuite[] = toTestSuites(input);
-        it("example test", () => {
-            const [css, TestSuitesComponent] = TestSuites({testSuites });
-            expect(testSuites).toHaveLength(2);
+        const [, TestSuitesComponent] = TestSuites({testSuites });
+        const element = ReactDomServer.renderToStaticMarkup(<TestSuitesComponent />);
+        const dom = new JSDOM(element);
+        it("should have fieldset", () => {
+            const fieldset = dom.window.document.querySelector("fieldset");
+            expect(fieldset).toBeInTheDocument();
+        });
+        it("should have two labels", () => {
+            const labels = dom.window.document.querySelectorAll("label");
+            expect(labels).toHaveLength(2);
+        });
+        it("rendered component should have label with expected class", () => {
+            const label = dom.window.document.querySelector("label .test-suite-label-dsl-test-ts");
+            expect(label).toBeDefined();
+        });
+        it("should have two divs for test suite results", () => {
+            const divs = dom.window.document.querySelectorAll("div .test-suite");
+            expect(divs).toHaveLength(2);
+        });
+        it("should have div with expected class", () => {
+            const div = dom.window.document.querySelector("div .test-suite-dsl-test-ts");
+            expect(div).toBeDefined();
         });
     });
 });
