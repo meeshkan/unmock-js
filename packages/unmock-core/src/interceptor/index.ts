d26272bd78864b3fc1e3271efe3e36925cc67a4e
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-11 16:00:25 +0300

    Split unmock into node, browser, react-native dists. (#312)
    Improve React Native.
    
    Fix tests.
    
    Fix tests more.
    
    Fix browser export.
    
    Add new package.
    
    Work on fetch-mitm.
    
    More work.
    
    Extract algorithm part.
    
    Rename package as unmock-fetch.
    
    Revert changes to unmock.
    
    More clean-up.
    
    Fix a bug in response body.
    
    Remove console.logs.
    
    More clean-up.
    
    Use types from unmock-core.
    
    Refactor types.
    
    Add tests, remove algorithm.
    
    Clean-up.
    
    Remove unused deps.
    
    Remove unnecessary test.

--- packages/unmock-core/src/interceptor/index.ts
+++ packages/unmock-core/src/interceptor/index.ts
@@ -1,16 +1,5 @@
-import { ISerializedRequest, ISerializedResponse } from "../interfaces";
+import { OnSerializedRequest } from "../interfaces";
 import NodeInterceptor from "./node-interceptor";
-
-/**
- * Interceptor callback. Called after request is serialized with
- * (1) serialized request, (2) function for sending serialized response,
- * (3) a function for emitting an error.
- */
-export type OnSerializedRequest = (
-  serializedRequest: ISerializedRequest,
-  sendResponse: (res: ISerializedResponse) => void,
-  emitError: (e: Error) => void,
-) => void;
 
 export interface IInterceptorOptions {
   onSerializedRequest: OnSerializedRequest;
---
+++ packages/unmock-fetch/src/serialize.ts
@@ -0,0 +1,61 @@
+import debug from "debug";
+import {
+  HTTPMethod,
+  IProtocol,
+  ISerializedRequest,
+} from "unmock-core/dist/interfaces";
+import URL = require("url-parse");
+
+const isKnownProtocol = (maybeProtocol: string): maybeProtocol is IProtocol =>
+  /^https?$/.test(maybeProtocol);
+
+// TODO Importing this from "unmock-core/dist/interfaces" does not work, it
+// probably needs to be properly exported from the top-level package
+export const isRESTMethod = (maybeMethod: string): maybeMethod is HTTPMethod =>
+  [
+    "get",
+    "head",
+    "post",
+    "put",
+    "patch",
+    "delete",
+    "options",
+    "trace",
+  ].includes(maybeMethod);
+
+const debugLog = debug("unmock:fetch-mitm");
+
+export default (url: RequestInfo, init?: RequestInit): ISerializedRequest => {
+  if (typeof url !== "string") {
+    throw new Error(`Request instance not yet supported`);
+  }
+
+  debugLog(`Serializing request to: ${url}`);
+
+  const method = (init && init.method && init.method.toLowerCase()) || "get";
+
+  if (!isRESTMethod(method)) {
+    throw new Error(`Unknown method: ${method}`);
+  }
+
+  const parsedUrl = new URL(url);
+
+  const protocolWithoutColon = parsedUrl.protocol.replace(":", "");
+
+  if (!isKnownProtocol(protocolWithoutColon)) {
+    throw new Error(`Unknown protocol: ${protocolWithoutColon}`);
+  }
+
+  const req: ISerializedRequest = {
+    method,
+    headers: {},
+    host: parsedUrl.host,
+    path: parsedUrl.pathname, // TODO
+    pathname: parsedUrl.pathname,
+    query: parsedUrl.query,
+    protocol: protocolWithoutColon,
+  };
+
+  debugLog(`Serialized request: ${JSON.stringify(req)}`);
+  return req;
+};
