4ec460dd7ace574a23e53eae56e96e80558cc329
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-11 16:13:44 +0300

    Remove unused console stuff.

--- packages/unmock-core/src/console/console.ts
+++ packages/unmock-core/src/console/console.ts
@@ -1,21 +1,9 @@
 import boxen, { Options as BoxenOptions } from "boxen";
-import { Console } from "console";
 import { flow } from "lodash/fp";
-import { format } from "util";
 import { Chalks } from "./chalks";
 import { LogMessage, LogType } from "./types";
 
 type Format = (type: LogType, message: LogMessage) => string;
-
-const indentRegex = /^(?!\s*$)/gm;
-const indentString = (s: string, count: number) =>
-  s.replace(indentRegex, " ".repeat(count));
-
-function clearLine(stream: NodeJS.WritableStream & { isTTY?: boolean }) {
-  if (stream.isTTY) {
-    stream.write("\x1b[999D\x1b[K");
-  }
-}
 
 const boxenOptions: BoxenOptions = {
   padding: 0,
@@ -39,32 +27,3 @@ const formatLogType: Format = (type: LogType, message: LogMessage): string =>
 export const formatMsg = (type: LogType, message: string): string => {
   return formatLogType(type, message);
 };
-
-export class CustomConsole extends Console {
-  private readonly stdout: NodeJS.WritableStream;
-  private readonly stderr: NodeJS.WritableStream;
-  private readonly nIndent = 2;
-  constructor(stdout: NodeJS.WritableStream, stderr: NodeJS.WritableStream) {
-    super(stdout, stderr);
-    this.stdout = stdout;
-    this.stderr = stderr;
-  }
-
-  public instruct(firstArg: any, ...args: any[]) {
-    this.logMessage("instruct", format(firstArg, ...args));
-  }
-
-  public error(firstArg: any, ...args: any[]) {
-    this.logError("error", format(firstArg, ...args));
-  }
-
-  private logMessage(type: LogType, message: string) {
-    clearLine(this.stdout);
-    super.log(indentString(formatMsg(type, message), this.nIndent));
-  }
-
-  private logError(type: LogType, message: string) {
-    clearLine(this.stderr);
-    super.error(indentString(formatMsg(type, message), this.nIndent));
-  }
-}
