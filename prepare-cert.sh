fa2eacdbd0b115d22e2bc1ae665a3e53773e7a44
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-16 14:49:11 +0300

    Add script for preparing certs.

---
+++ prepare-cert.sh
@@ -0,0 +1,29 @@
+string=$1
+
+if [ -z ${string} ];
+then
+  echo "Add domain names like this: bash prepare-cert.sh domain.io,domain.io2"
+  exit 1
+fi
+
+arr=(${string//,/ })
+
+SAN=""
+
+first=true
+
+for i in "${arr[@]}"
+do
+   first=false
+   echo "$i"
+   SAN+=DNS:$i
+   if [ ! first ];
+   then
+    SAN+=,
+   fi
+   echo $SAN
+done
+
+openssl req -x509 -newkey rsa:4096 -subj "/C=FI/ST=HE/O=Meeshkan/CN=*" -reqexts SAN -extensions SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=${SAN}")) -nodes -keyout key.pem -out cert.pem
+
+echo $(openssl x509 -in cert.pem -text -noout)
