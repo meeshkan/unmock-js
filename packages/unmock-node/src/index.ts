0b87384bbfca112a7e0ca946ad817e0bd142b9d0
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-16 11:34:21 +0300

    Delete server.ts for now.

--- packages/unmock-node/src/index.ts
+++ packages/unmock-node/src/index.ts
@@ -3,7 +3,6 @@ import { UnmockPackage } from "unmock-core";
 import NodeBackend from "./backend";
 import FsSnapshotter from "./loggers/snapshotter";
 import WinstonLogger from "./loggers/winston-logger";
-import ServerBackend from "./server";
 
 const utils = { snapshotter: FsSnapshotter };
 export { utils };
@@ -16,8 +15,5 @@ export const unmock = new UnmockPackage(new NodeBackend(), {
 
 export const nock = unmock.nock.bind(unmock);
 export const runner = unmock.runner.bind(unmock);
-
-const serverUrl = "http://localhost:9000";
-export const server = new UnmockPackage(new ServerBackend({ url: serverUrl }));
 
 export default unmock;
