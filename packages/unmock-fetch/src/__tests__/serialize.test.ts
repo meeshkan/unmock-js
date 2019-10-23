68c37b054c547c59fda85b01f9ed39fea0d7d1ad
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-23 14:20:16 +0300

    Add tests that break.

---
+++ packages/unmock-fetch/src/__tests__/serialize.test.ts
@@ -0,0 +1,26 @@
+import { ISerializedRequest } from "unmock-core";
+import serialize from "../serialize";
+
+describe("Fetch request serializer", () => {
+  it("should serialize a plain request URL", () => {
+    const req: ISerializedRequest = serialize("https://example.com/v1?q=a");
+    expect(req).toMatchObject({
+      host: "example.com",
+      path: "/v1?q=a",
+      pathname: "/v1",
+      query: { q: "a" },
+      method: "get",
+      protocol: "https",
+    });
+  });
+
+  it("should serialize a request with headers", () => {
+    const headers = { "x-accept": "anything " };
+    const req: ISerializedRequest = serialize("https://example.com", {
+      headers,
+    });
+    expect(req).toMatchObject({
+      headers,
+    });
+  });
+});
