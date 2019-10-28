2144489df5f5fbd1c427e085e883a3fefc148a57
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-28 09:37:25 +0200

    Add tests for AllowedHosts.

--- packages/unmock-core/src/__tests__/settings.test.ts
+++ packages/unmock-core/src/__tests__/settings.test.ts
@@ -1,4 +1,4 @@
-import { BooleanSetting } from "../settings";
+import { AllowedHosts, BooleanSetting } from "../settings";
 
 describe("Boolean setting", () => {
   it("should be false by default", () => {
@@ -9,5 +9,22 @@ describe("Boolean setting", () => {
     const booleanSetting = new BooleanSetting();
     booleanSetting.on();
     expect(booleanSetting.get()).toBe(true);
+  });
+});
+
+describe("Allowed hosts list", () => {
+  it("should include localhost", () => {
+    const allowedHosts = new AllowedHosts();
+    expect(allowedHosts.isWhitelisted("localhost")).toBe(true);
+  });
+  it("should add given URL", () => {
+    const allowedHosts = new AllowedHosts();
+    allowedHosts.add("unmock.io");
+    expect(allowedHosts.isWhitelisted("unmock.io")).toBe(true);
+  });
+  it("should add URL with regex", () => {
+    const allowedHosts = new AllowedHosts();
+    allowedHosts.add(/unmock/);
+    expect(allowedHosts.isWhitelisted("unmock.io")).toBe(true);
   });
 });
