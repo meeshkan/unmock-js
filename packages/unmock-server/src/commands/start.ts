787c8d6de16d2663c0e5b81a1e5362bf0e0b2dc6
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-11-20 15:24:11 +0200

    Clean-up.

--- packages/unmock-server/src/commands/start.ts
+++ packages/unmock-server/src/commands/start.ts
@@ -12,20 +12,11 @@ export default class Start extends Command {
 
   public static flags = {
     help: flags.help({ char: "h" }),
-    // flag with a value (-n, --name=VALUE)
-    /* name: flags.string({
-      char: "n",
-      description: "name to print",
-    }), */
-    // flag with no value (-f, --force)
-    // force: flags.boolean({ char: "f" }),
   };
 
   public static args = [];
 
   public async run() {
-    // const { args, flags: flagz } = this.parse(Start);
-
     const run = () => {
       const { app } = buildApp();
       startServer(app);
