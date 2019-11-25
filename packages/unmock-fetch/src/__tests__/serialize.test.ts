6c5ead4b00b363af810688c485febe3b87a59f28
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-11-25 10:02:43 +0200

    Add new tests.

--- packages/unmock-fetch/src/__tests__/serialize.test.ts
+++ packages/unmock-fetch/src/__tests__/serialize.test.ts
@@ -1,5 +1,6 @@
 import { ISerializedRequest } from "unmock-core";
 import serialize from "../serialize";
+import { Headers } from "../types";
 
 describe("Fetch request serializer", () => {
   it("should serialize method correctly", () => {
@@ -21,13 +22,38 @@ describe("Fetch request serializer", () => {
     });
   });
 
-  it("should serialize a request with headers", () => {
-    const headers = { "x-accept": "anything " };
-    const req: ISerializedRequest = serialize("https://example.com", {
-      headers,
+  describe("header serialization", () => {
+    it("should serialize a request with headers dictionary", () => {
+      const headers = { "x-accept": "anything" };
+      const req: ISerializedRequest = serialize("https://example.com", {
+        headers,
+      });
+      expect(req).toMatchObject({
+        headers,
+      });
     });
-    expect(req).toMatchObject({
-      headers,
+    it("should normalize headers keys", () => {
+      const headers = { "X-ACCEPT": "anything" };
+      const req: ISerializedRequest = serialize("https://example.com", {
+        headers,
+      });
+      expect(req).toMatchObject({
+        headers: {
+          "x-accept": "anything",
+        },
+      });
+    });
+    it("should serialize a request with headers object", () => {
+      const headers = new Headers();
+      headers.append("x-accept", "anything");
+      const req: ISerializedRequest = serialize("https://example.com", {
+        headers,
+      });
+      expect(req).toMatchObject({
+        headers: {
+          "x-accept": "anything",
+        },
+      });
     });
   });
 });
