f1cfc6cc455053e8dea9ec6c1a7a4997c019c59f
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-12-16 13:40:35 +0200

    Move polyfill to unmock-browser.

--- packages/unmock/src/browser/index.ts
+++ packages/unmock/src/browser/index.ts
@@ -1,9 +1,3 @@
-/**
- * Polyfill Buffer if undefined, required by URL operations
- */
-if (typeof Buffer === "undefined") {
-  global.Buffer = require("buffer").Buffer; // tslint:disable-line:no-var-requires
-}
 export * from "unmock-core";
 import unmock from "unmock-browser";
 export * from "unmock-browser";
