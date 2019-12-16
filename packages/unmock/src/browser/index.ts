6009a9a29bafc7156c4d3523677a173846551bc1
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-12-16 13:27:27 +0200

    Polyfill Buffer in unmock-browser.

--- packages/unmock/src/browser/index.ts
+++ packages/unmock/src/browser/index.ts
@@ -1,3 +1,9 @@
+/**
+ * Polyfill Buffer if undefined, required by URL operations
+ */
+if (typeof Buffer === "undefined") {
+  global.Buffer = require("buffer").Buffer; // tslint:disable-line:no-var-requires
+}
 export * from "unmock-core";
 import unmock from "unmock-browser";
 export * from "unmock-browser";
