4bb7b8bd82d0f4d900bbaa1adcdccda3755c1cdc
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-01-24 14:59:58 +0200

    Add stop command by writing server PID to file.

--- packages/unmock-server/src/commands/start.ts
+++ packages/unmock-server/src/commands/start.ts
@@ -1,6 +1,10 @@
 import { Command, flags } from "@oclif/command";
+import debug from "debug";
+import { writePid } from "../pid";
 import { startProxy } from "../proxy";
 import { buildApp, startServer } from "../server";
+
+const debugLog = debug("unmock-server:start");
 
 export default class Start extends Command {
   public static description = "Start unmock server and proxy";
@@ -17,10 +21,23 @@ export default class Start extends Command {
   public static args = [];
 
   public async run() {
+    debugLog('Starting.');
     const run = () => {
       const { app } = buildApp();
-      startServer(app);
-      startProxy();
+      const [httpServer, httpsServer] = startServer(app);
+      const proxyServer = startProxy();
+
+      const sigTermHandler = () => {
+        debugLog('Received SIGTERM. Stopping servers.');
+        httpServer.close();
+        httpsServer.close();
+        proxyServer.close();
+        debugLog('Servers closed.');
+      }
+
+      process.on('SIGTERM', sigTermHandler);
+      debugLog("Writing PID to file for closing...");
+      writePid();
     };
     run();
   }
---
+++ packages/unmock-server/src/commands/stop.ts
@@ -0,0 +1,46 @@
+import { Command, flags } from "@oclif/command";
+import debug from "debug";
+import { deletePidFile, readPidIfExists, TERM_SIGNAL } from "../pid";
+
+const debugLog = debug("unmock-server:stop");
+const log = (...args: any[]) => console.log(...args); // tslint:disable-line
+
+export default class Stop extends Command {
+  public static description = "Stop unmock server and proxy.";
+
+  public static examples = [
+    `$ unmock-server stop
+`,
+  ];
+
+  public static flags = {
+    help: flags.help({ char: "h" }),
+  };
+
+  public static args = [];
+
+  /**
+   * Server process is stopped by sending a SIGTERM to the process PID written to the file-system.
+   */
+  public async run() {
+    const pidOrNull = readPidIfExists();
+
+    if (pidOrNull === null) {
+        log("Server not running.")
+        return;
+    }
+
+    try {
+        process.kill(pidOrNull, 0);
+    } catch (ex) {
+        log("Server not running.");
+        return;
+    }
+
+    debugLog("Sending %s to PID %d", TERM_SIGNAL, pidOrNull);
+    process.kill(pidOrNull, TERM_SIGNAL);
+    log("Server stopped.");
+
+    deletePidFile();
+  }
+}
