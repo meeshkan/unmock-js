6cd26e58d6e1a758816f162545b81a9ce6c5927a
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-01-24 15:12:27 +0200

    Fix prettier.

--- packages/unmock-server/src/__tests__/pid.test.ts
+++ packages/unmock-server/src/__tests__/pid.test.ts
@@ -4,12 +4,12 @@ import { deletePidFile, readPidIfExists, writePid } from "../pid";
 const localConfigDirectory = path.resolve(__dirname, ".unmock");
 
 afterEach(() => {
-    deletePidFile();
+  deletePidFile();
 });
 
 it("should write process PID to given file and read it", () => {
-    const pid = process.pid;
-    writePid(localConfigDirectory);
-    const writtenPid = readPidIfExists(localConfigDirectory);
-    expect(writtenPid).toBe(pid);
+  const pid = process.pid;
+  writePid(localConfigDirectory);
+  const writtenPid = readPidIfExists(localConfigDirectory);
+  expect(writtenPid).toBe(pid);
 });
