0f9f4e36fae6f532e8f32d883694acdd55d316aa
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-01-24 15:13:03 +0200

    Fix deletion in test.

--- packages/unmock-server/src/__tests__/pid.test.ts
+++ packages/unmock-server/src/__tests__/pid.test.ts
@@ -1,15 +1,15 @@
 import * as path from "path";
 import { deletePidFile, readPidIfExists, writePid } from "../pid";
 
-const localConfigDirectory = path.resolve(__dirname, ".unmock");
+const LOCAL_CONFIG_DIRECTORY = path.resolve(__dirname, ".unmock");
 
 afterEach(() => {
-  deletePidFile();
+  deletePidFile(LOCAL_CONFIG_DIRECTORY);
 });
 
 it("should write process PID to given file and read it", () => {
   const pid = process.pid;
-  writePid(localConfigDirectory);
-  const writtenPid = readPidIfExists(localConfigDirectory);
+  writePid(LOCAL_CONFIG_DIRECTORY);
+  const writtenPid = readPidIfExists(LOCAL_CONFIG_DIRECTORY);
   expect(writtenPid).toBe(pid);
 });
